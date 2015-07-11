14ceee00f509d80f977a3eb1c118000f8f48615b
Tony Miller <mcfiredrill@gmail.com>
2015-07-11 12:14:02 +0900

    clear out html before setting it again

--- app/js/main.js
+++ app/js/main.js
@@ -239,6 +239,12 @@ $(function(){
       editable: true,
       eventSources: ["http://datafruits.streampusher.com/scheduled_shows.json"],
       eventClick: function(calEvent, jsEvent, view) {
+        $("#eventModal h1").html("");
+        $("#eventModal h2").html("");
+        $("#eventModal .start-time").html("");
+        $("#eventModal .end-time").html("");
+        $("#eventModal .event-image").html("");
+
         $("#timetableModal").modal('hide');
         $("#eventModal h1").html(calEvent.title);
         $("#eventModal h2").html(calEvent.dj.username);
