f6e57ddc2e87695dc74fcd6f731d82d472358203
Tony Miller <mcfiredrill@gmail.com>
2015-08-21 22:56:12 -0700

    setup jplayer on didInsertElement in application view

--- datafruits/app/app.js
+++ datafruits/app/app.js
@@ -10,7 +10,16 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+  ready: function() {
+    setTimeout(function(){
+      radioTitle();
+    }, 500);
+
+    setInterval(function(){
+      radioTitle();
+    }, 10000);
+  }
 });
 
 loadInitializers(App, config.modulePrefix);
@@ -27,7 +36,6 @@ var entityMap = {
 };
 
 function escapeHtml(string) {
-  //return String(string).replace(/[&<>"'\/]/g, function (s) {
   return String(string).replace(/[<>"']/g, function (s) {
     return entityMap[s];
   });
@@ -45,54 +53,3 @@ function radioTitle(){
     $('.jp-title').html(title);
   });
 }
-
-$(function(){
-  var stream = {
-    mp3: 'http://datafruits.streampusher.com:8000/datafruits.mp3',
-    oga: 'http://datafruits.streampusher.com:8000/datafruits.ogg'
-  };
-
-  var playButtonClicked = false;
-  $('#radio-player').jPlayer({
-    ready: function () {
-      $(this).jPlayer('setMedia', stream);
-    },
-    supplied: 'mp3, oga',
-    wmode: 'window',
-    playing: function(e) {
-      $('.jp-loading').hide();
-    },
-    pause: function(e){
-      $(this).jPlayer('clearMedia');
-      $(this).jPlayer('setMedia', stream);
-    },
-    error: function(event) {
-      console.log('jPlayer error: '+ event.jPlayer.error.type);
-
-      if(playButtonClicked === true){
-        $(this).jPlayer('setMedia', stream).jPlayer('play');
-      }
-
-      $('jp-pause').hide();
-      $('jp-loading').hide();
-    },
-    waiting: function(e) {
-      $('.jp-loading').show();
-      $('.jp-play').hide();
-      $('.jp-pause').hide();
-    },
-    loadeddata: function(e) {
-      $('.jp-loading').hide();
-    },
-    solution: 'html, flash',
-    cssSelectorAncestor: '#jp_container'
-  });
-
-  setTimeout(function(){
-    radioTitle();
-  }, 500);
-
-  setInterval(function(){
-    radioTitle();
-  }, 10000);
-});
---
+++ datafruits/app/views/application.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+
+function radioTitle(){
+  var url = $('#radio-player').data('icecast-json').toString();
+  console.log(url);
+
+  $.get(url, function(data){
+    var title = data.icestats.source[0].title;
+    console.log(title);
+    $('.jp-title').html(title);
+  });
+}
+
+export default Ember.View.extend({
+  setupJplayer: function(){
+    console.log("setting up jPlayer");
+    var stream = {
+      mp3: 'http://datafruits.streampusher.com:8000/datafruits.mp3',
+      oga: 'http://datafruits.streampusher.com:8000/datafruits.ogg'
+    };
+
+    var playButtonClicked = false;
+    $('#radio-player').jPlayer({
+      ready: function () {
+        $(this).jPlayer('setMedia', stream);
+      },
+      supplied: 'mp3, oga',
+      wmode: 'window',
+      playing: function(e) {
+        $('.jp-loading').hide();
+      },
+      pause: function(e){
+        $(this).jPlayer('clearMedia');
+        $(this).jPlayer('setMedia', stream);
+      },
+      error: function(event) {
+        console.log('jPlayer error: '+ event.jPlayer.error.type);
+
+        if(playButtonClicked === true){
+          $(this).jPlayer('setMedia', stream).jPlayer('play');
+        }
+
+        $('jp-pause').hide();
+        $('jp-loading').hide();
+      },
+      waiting: function(e) {
+        $('.jp-loading').show();
+        $('.jp-play').hide();
+        $('.jp-pause').hide();
+      },
+      loadeddata: function(e) {
+        $('.jp-loading').hide();
+      },
+      solution: 'html, flash',
+      cssSelectorAncestor: '#jp_container'
+    });
+
+  }.on('didInsertElement')
+});
