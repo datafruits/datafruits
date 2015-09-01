5f8daae32775c27f7acc75b40da843da49f401df
Tony Miller <mcfiredrill@gmail.com>
2015-09-01 14:10:06 +0900

    use embercligoogleanalytics

--- datafruits/config/environment.js
+++ datafruits/config/environment.js
@@ -42,7 +42,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.googleAnalytics = {
+      webPropertyId: 'UA-28868734-1'
+    };
   }
 
   if (environment == 'staging') {
