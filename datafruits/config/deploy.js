aca39be411c9ba9d5ff05d8095fb5062daf5f9c7
Tony Miller <mcfiredrill@gmail.com>
2015-09-01 13:50:02 +0900

    add production to deploy

--- datafruits/config/deploy.js
+++ datafruits/config/deploy.js
@@ -13,5 +13,21 @@ module.exports = {
       secretAccessKey: process.env.S3_SECRET,
       bucket: "datafruitstest"
     }
+  },
+  production: {
+    store: {
+      type: "S3",
+      accessKeyId: process.env.S3_KEY,
+      secretAccessKey: process.env.S3_SECRET,
+      bucket: "datafruits.fm-index"
+    },
+
+    assets: {
+      type: "s3",
+      accessKeyId: process.env.S3_KEY,
+      secretAccessKey: process.env.S3_SECRET,
+      bucket: "datafruits.fm"
+    }
   }
+
 }
