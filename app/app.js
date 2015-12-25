46fe2cf24b19f79f96bd9203b729b65c735f0681
Tony Miller <mcfiredrill@gmail.com>
2015-12-26 02:42:02 +0900

    bye bye twitch

--- app/app.js
+++ app/app.js
@@ -10,16 +10,7 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver,
-  ready: function() {
-    setTimeout(function(){
-      checkTwitch();
-    }, 500);
-
-    setInterval(function(){
-      checkTwitch();
-    }, 2500);
-  }
+  Resolver: Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
@@ -42,34 +33,3 @@ function escapeHtml(string) {
 }
 
 var imgRegex = /https?:\/\/(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpg|gif|png)/;
-
-var vjIsLive = false;
-var showingVj = false;
-
-function showVJ(){
-  showingVj = true;
-  $('#livestream').show();
-}
-
-function hideVJ(){
-  showingVj = false;
-  $('#livestream').hide();
-}
-
-function checkTwitch(){
-  $.getJSON('https://api.twitch.tv/kraken/streams/datafruits.json?callback=?', function(data){
-    if(data.stream === null) {
-      console.log('no live VJ');
-      vjIsLive = false;
-      if(showingVj === true){
-        hideVJ();
-      }
-    }else {
-      console.log('live stream!');
-      vjIsLive = true;
-      if(showingVj === false){
-        showVJ();
-      }
-    }
-  });
-}
