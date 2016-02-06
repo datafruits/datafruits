5fc29656ef02a7e943cccf8905c61859bcdba704
Tony Miller <mcfiredrill@gmail.com>
2016-02-06 14:47:01 +0900

    make youtube embed responsive, remove controls

--- app/components/datafruits-visuals.js
+++ app/components/datafruits-visuals.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  playerVars: {
+    autoplay: 0,
+    controls: 0,
+    enablejsapi: 1,
+    rel: 0, // disable related videos
+    showinfo: 0,
+    autohide: 1,
+    fs: 0, // disable fullscreen button
+    playsinline: 1,
+    disablekb: 1,
+    // iv_load_policy: 3,
+    // modestbranding: 1,
+  },
+
   classNames: ['visuals'],
   pollVjApi: function(){
     var _this = this;
