ea89dc4345f960d94df842540d7d1dec79af086d
Tony Miller <mcfiredrill@gmail.com>
2016-03-13 10:34:35 +0900

    finally show the next live on air link

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return Ember.$.getJSON('http://datafruits.streampusher.com/scheduled_shows/next.json')
+    .then(function(data){
+      return data;
+    });
+  }
 });
