7d457d66549e07b96633748f15a7c7e717bbe951
Tony Miller <mcfiredrill@gmail.com>
2016-05-27 18:22:15 +0900

    remove unused code in app.js

--- app/app.js
+++ app/app.js
@@ -16,20 +16,3 @@ App = Ember.Application.extend({
 loadInitializers(App, config.modulePrefix);
 
 export default App;
-
-var entityMap = {
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': '&quot;',
-  "'": '&#39;',
-  "/": '&#x2F;'
-};
-
-function escapeHtml(string) {
-  return String(string).replace(/[<>"']/g, function (s) {
-    return entityMap[s];
-  });
-}
-
-var imgRegex = /https?:\/\/(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpg|gif|png)/;
