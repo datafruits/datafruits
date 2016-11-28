154e63a5fcb1ccd4ca1dc78654ada422e7bdd874
Tony Miller <mcfiredrill@gmail.com>
2016-11-28 12:24:09 +0900

    fix indentation

--- app/components/datafruits-chat.js
+++ app/components/datafruits-chat.js
@@ -123,44 +123,43 @@ export default Ember.Component.extend({
     });
 
     $("#input-message").textcomplete([ {
-        match: /\B:([\-+\w]*)$/,
-        search: function (term, callback) {
-            var results = [];
-            var results2 = [];
-            var results3 = [];
-            $.each(emojiStrategy,function(shortname,data) {
-                if(shortname.indexOf(term) > -1) { results.push(shortname); }
-                else {
-                    if((data.aliases !== null) && (data.aliases.indexOf(term) > -1)) {
-                        results2.push(shortname);
-                    }
-                    else if((data.keywords !== null) && (data.keywords.indexOf(term) > -1)) {
-                        results3.push(shortname);
-                    }
-                }
-            });
- 
-            if(term.length >= 3) {
-                results.sort(function(a,b) { return (a.length > b.length); });
-                results2.sort(function(a,b) { return (a.length > b.length); });
-                results3.sort();
-            }
-            var newResults = results.concat(results2).concat(results3);
- 
-            callback(newResults);
-        },
-        template: function (shortname) {
-            return '<img class="emojione" src="//cdn.jsdelivr.net/emojione/assets/png/'+emojiStrategy[shortname].unicode+'.png"> :'+shortname+':';
-        },
-        replace: function (shortname) {
-            return ':'+shortname+': ';
-        },
-        index: 1,
-        maxCount: 10
-    }],{
-        footer: '<a href="http://www.emoji.codes" target="_blank">Browse All<span class="arrow">»</span></a>',
-        className: 'emoji-autocomplete'
-    });
+      match: /\B:([\-+\w]*)$/,
+      search: function (term, callback) {
+      var results = [];
+      var results2 = [];
+      var results3 = [];
+      $.each(emojiStrategy,function(shortname,data) {
+        if(shortname.indexOf(term) > -1) { results.push(shortname); }
+        else {
+          if((data.aliases !== null) && (data.aliases.indexOf(term) > -1)) {
+            results2.push(shortname);
+          }
+          else if((data.keywords !== null) && (data.keywords.indexOf(term) > -1)) {
+            results3.push(shortname);
+          }
+        }
+      });
+      if(term.length >= 3) {
+        results.sort(function(a,b) { return (a.length > b.length); });
+        results2.sort(function(a,b) { return (a.length > b.length); });
+        results3.sort();
+      }
+      var newResults = results.concat(results2).concat(results3);
+
+      callback(newResults);
+      },
+      template: function (shortname) {
+        return '<img class="emojione" src="//cdn.jsdelivr.net/emojione/assets/png/'+emojiStrategy[shortname].unicode+'.png"> :'+shortname+':';
+      },
+      replace: function (shortname) {
+        return ':'+shortname+': ';
+      },
+      index: 1,
+      maxCount: 10
+    }],{
+      footer: '<a href="http://www.emoji.codes" target="_blank">Browse All<span class="arrow">»</span></a>',
+      className: 'emoji-autocomplete'
+    });
 
     var onScroll = this._onScroll.bind(this);
     this.$("#messages").bind('touchmove', onScroll);
