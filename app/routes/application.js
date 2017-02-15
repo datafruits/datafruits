db8d3c28aa2a6316a55c17dcd18b09af98f3309c
Tony Miller <mcfiredrill@gmail.com>
2017-02-15 18:09:00 +0900

    use secure endpoint for next.json

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return Ember.$.getJSON('http://datafruits.streampusher.com/scheduled_shows/next.json')
+    return Ember.$.getJSON('https://datafruits.streampusher.com/scheduled_shows/next.json')
     .then(function(data){
       return data;
     });
