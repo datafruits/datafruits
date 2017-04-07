ebc4a3afd2ff51751635a1cbabd4eeb7bc533175
Tony Miller <mcfiredrill@gmail.com>
2017-04-07 17:15:59 +0900

    add rel "noopener noreferrer" to outgoing links
    https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/

--- app/helpers/format-message-body.js
+++ app/helpers/format-message-body.js
@@ -3,7 +3,7 @@ import emojione from 'npm:emojione';
 
 export function formatMessageBody(params/*, hash*/) {
   let message = Ember.Handlebars.Utils.escapeExpression(params[0]);
-  return Ember.String.htmlSafe(emojione.shortnameToImage(message).autoLink({ target: "_blank", }));
+  return Ember.String.htmlSafe(emojione.shortnameToImage(message).autoLink({ target: "_blank", rel: "noopener noreferrer" }));
 }
 
 export default Ember.Helper.helper(formatMessageBody);
