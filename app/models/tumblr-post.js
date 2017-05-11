864dd2b05ca0dfe9376f001e2d3411f4b5314bbc
Tony Miller <mcfiredrill@gmail.com>
2017-05-11 11:15:25 +0900

    display tumblr video posts

--- app/models/tumblr-post.js
+++ app/models/tumblr-post.js
@@ -5,8 +5,12 @@ import DS from 'ember-data';
 export default TumblrPost.extend({
   photos: DS.attr(),
   caption: DS.attr(),
+  player: DS.attr(),
   captionSafe: Ember.computed('caption', function(){
     return Ember.String.htmlSafe(this.get('caption'));
+  }),
+  playerSafe: Ember.computed('player', function(){
+    return Ember.String.htmlSafe(this.get('player')[2].embed_code);
   }),
   body: DS.attr(),
   bodySafe: Ember.computed('body', function(){
