f2a91908b5a0a81ec67c7fb8bbc6c2eb0ff842a8
Tony Miller <mcfiredrill@gmail.com>
2017-05-29 16:54:09 +0900

    show audio posts

--- app/models/tumblr-post.js
+++ app/models/tumblr-post.js
@@ -12,7 +12,11 @@ export default TumblrPost.extend({
   playerSafe: Ember.computed('player', function(){
     return Ember.String.htmlSafe(this.get('player')[2].embed_code);
   }),
+  embedSafe: Ember.computed('embed', function(){
+    return Ember.String.htmlSafe(this.get('embed'));
+  }),
   body: DS.attr(),
+  embed: DS.attr(),
   bodySafe: Ember.computed('body', function(){
     return Ember.String.htmlSafe(this.get('body'));
   }),
