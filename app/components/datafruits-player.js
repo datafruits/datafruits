4bb2fc44b3546a75aff5fcd1a2346b5798b0ec34
Tony Miller <mcfiredrill@gmail.com>
2017-06-27 11:13:02 +0900

    fix datafruits-player component

--- app/components/datafruits-player.js
+++ app/components/datafruits-player.js
@@ -5,19 +5,19 @@ export default Ember.Component.extend({
   _initialize: Ember.on('init', function(){
     this.get("eventBus").subscribe("trackPlayed", this, "onTrackPlayed");
   }),
-  pollRadioTitle: function() {
-    var _this = this;
+  pollRadioTitle() {
+    let _this = this;
     Ember.run.later(function() {
       _this.setRadioTitle();
       _this.pollRadioTitle();
     }, 10000);
   },
-  setRadioTitle: function(){
+  setRadioTitle() {
     if(!this.get('playingPodcast')){
-      var url = $('#radio-player').data('icecast-json').toString();
+      let url = $('#radio-player').data('icecast-json').toString();
 
-      $.get(url, function(data){
-        var datafruits = data.icestats.source.find((s) => {
+      $.get(url, (data) => {
+        let datafruits = data.icestats.source.find((s) => {
           return s.server_name == "datafruits.mp3";
         });
         let title = datafruits.title;
@@ -25,14 +25,14 @@ export default Ember.Component.extend({
           title = title.slice(3);
         }
         //$('.jp-title').html(title);
-        this.clear('error');
+        this.set('error', null);
         this.set('title', title);
       });
     }
   },
-  onTrackPlayed: function(track){
+  onTrackPlayed(/*track*/){
     //$('.jp-title').html(track.title);
-    this.clear('error');
+    this.set('error', null);
     this.set('title', title);
     this.set('playingPodcast', true);
   },
@@ -49,13 +49,13 @@ export default Ember.Component.extend({
   playingPodcast: false,
   eventBus: Ember.inject.service(),
   setup: function(){
-    var _this = this;
+    let _this = this;
     this.stream = {
       mp3: 'https://streampusher-relay.club/datafruits.mp3',
       oga: 'https://streampusher-relay.club/datafruits.ogg'
     };
 
-    var playTry = false;
+    let playTry = false;
     $('#radio-player').jPlayer({
       ready: function () {
         $(this).jPlayer('setMedia', _this.stream).jPlayer("play");
@@ -74,7 +74,7 @@ export default Ember.Component.extend({
       },
       error: function(/*event*/) {
         /*console.log('jPlayer error: '+ event.jPlayer.error.type);*/
-        this.set('error', "There was an error playing the stream. Trying again in a second...");
+        _this.set('error', "There was an error playing the stream. Trying again in a second...");
 
         $('jp-pause').hide();
         $('jp-loading').hide();
