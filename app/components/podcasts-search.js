5c6c64bfcf75acd605f3da5eb50f980dd0ded4e7
Tony Miller <mcfiredrill@gmail.com>
2017-07-10 13:05:02 +0900

    add search by labels

--- app/components/podcasts-search.js
+++ app/components/podcasts-search.js
@@ -1,13 +1,25 @@
 import Ember from 'ember';
+import _ from 'lodash';
 
 export default Ember.Component.extend({
   filterText: '',
-  isSearching: Ember.computed('filterText', function() {
-    return this.get('filterText') !== "";
+  selectedLabels: [],
+  isSearching: Ember.computed('filterText', 'selectedLabels', function() {
+    return this.get('filterText') !== "" || this.get('selectedLabels').length !== 0;
   }),
-  filteredResults: Ember.computed('filterText', function() {
+  filteredResults: Ember.computed('filterText', 'selectedLabels', function() {
     let filter = this.get('filterText');
+    let labelNames = this.get('selectedLabels').map(function(label){
+      return label.name;
+    });
     return this.get('podcasts').filter(function(item) {
+      if(labelNames.length != 0){
+        if(_.intersection(item.labels, labelNames).length == labelNames.length){
+          return item.title.toLowerCase().indexOf(filter) !== -1;
+        }else {
+          return false;
+        }
+      }
       return item.title.toLowerCase().indexOf(filter) !== -1;
     });
   }),
--- app/routes/podcasts.js
+++ app/routes/podcasts.js
@@ -3,11 +3,19 @@ import fetch from 'ember-network/fetch';
 
 export default Ember.Route.extend({
   model(){
-    return fetch('https://datafruits.streampusher.com/podcasts/datafruits.json')
-    .then(function(response){
-      return response.json().then(function(json){
-        return json.podcast;
-      });
+    return Ember.RSVP.hash({
+      podcasts: fetch('https://datafruits.streampusher.com/podcasts/datafruits.json')
+        .then(function(response){
+          return response.json().then(function(json){
+            return json.podcast.tracks;
+          });
+        }),
+      labels: fetch('https://datafruits.streampusher.com/labels.json')
+        .then(function(response){
+          return response.json().then(function(json){
+            return json.labels;
+          });
+        })
     });
   }
 });
