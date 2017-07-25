2a84ce7f3d2ce04fb4b54ffe1b81c449b232f857
Tony Miller <mcfiredrill@gmail.com>
2017-07-25 16:53:33 +0900

    don't run masonry in fastboot

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -1,21 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  fastboot: Ember.inject.service(),
   model: function(){
     return this.store.findAll('tumblr-post');
   },
   setupController: function(controller, model){
     this._super(controller, model);
     Ember.run.schedule('afterRender', this, function () {
-      $('.grid').masonry({
-	// options
-	itemSelector: '.grid-item',
-	columnWidth: 650,
-        gutter: 20
-      });
-      $('.grid').imagesLoaded(function(){
-        $(".grid").masonry();
-      });
+      if(!this.get('fastboot.isFastBoot')){
+        $('.grid').masonry({
+          // options
+          itemSelector: '.grid-item',
+          columnWidth: 650,
+          gutter: 20
+        });
+        $('.grid').imagesLoaded(function(){
+          $(".grid").masonry();
+        });
+      }
     });
   }
 });
