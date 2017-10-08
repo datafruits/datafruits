49538d3a7f5a98810ebd488d747a3eb781a9c922
Tony Miller <mcfiredrill@gmail.com>
2017-10-08 15:42:05 +0900

    add more emojis

--- app/initializers/custom-emoji.js
+++ app/initializers/custom-emoji.js
@@ -2,13 +2,31 @@ import emojione from 'npm:emojione';
 
 export function initialize(/* application */) {
   // application.inject('route', 'foo', 'service:foo');
-  emojione.emojioneList[":bgs:"] = {
-    custom: true,
-    uc_base: "bgs",
-    uc_output: "",
-    shortnames: []
+  const customEmojis = {
+    ":bgs:": {
+      custom: true,
+      uc_base: "bgs",
+      uc_output: "",
+      shortnames: []
+    },
+    ":tony:": {
+      custom: true,
+      uc_base: "tony",
+      uc_outpuut: "",
+      shortnames: [":firedrill:", ":freedrull:", ":mcfiredrill:"]
+    },
+    ":ovenrake:": {
+      custom: true,
+      uc_base: "ovenrake",
+      uc_outpuut: "",
+      shortnames: [":dakota:", ":oven:"]
+    }
   };
-  emojione.shortnames = emojione.shortnames.concat("|:bgs:");
+  Object.keys(customEmojis).forEach(emoji => {
+    emojione.emojioneList[emoji] = customEmojis[emoji];
+  });
+  emojione.shortnames = emojione.shortnames.concat(`|${Object.keys(customEmojis).join('|')}`);
+  //emojione.shortnames = emojione.shortnames.concat("|:bgs:");
   emojione.regShortNames = new RegExp("<object[^>]*>.*?<\/object>|<span[^>]*>.*?<\/span>|<(?:object|embed|svg|img|div|span|p|a)[^>]*>|("+emojione.shortnames+")", "gi");
 }
 
