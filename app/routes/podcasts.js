e6a4e2186fc56b15765aadf5895fe424bbd5fb0a
Tony Miller <mcfiredrill@gmail.com>
2018-03-10 12:22:25 +0900

    remove comment

--- app/routes/podcasts.js
+++ app/routes/podcasts.js
@@ -10,23 +10,4 @@ export default Ember.Route.extend({
       labels: this.store.findAll('label')
     });
   }
-  // model(){
-  //   return Ember.RSVP.hash({
-  //     podcasts: fetch('https://datafruits.streampusher.com/podcasts/datafruits.json')
-  //       .then(function(response){
-  //         return response.json().then(function(json){
-  //           return json.podcast.tracks;
-  //         });
-  //       }),
-  //     labels: fetch('https://datafruits.streampusher.com/labels.json')
-  //       .then(function(response){
-  //         return response.json().then(function(json){
-  //           let labelNames = json.labels.map(function(label){
-  //             return label.name;
-  //           });
-  //           return labelNames;
-  //         });
-  //       })
-  //   });
-  // }
 });
