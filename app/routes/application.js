31c34078cf58b6092b03d14ce7baccbe18833965
Tony Miller <mcfiredrill@gmail.com>
2018-03-10 12:13:15 +0900

    uncomment

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,16 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model(){
-  //   return this.store.queryRecord('scheduled-show', {next: true});
-  // },
-  // afterModel(){
-  //   this.refreshNext();
-  // },
-  // refreshNext() {
-  //   this.model();
-  //   Ember.run.later(() => {
-  //     this.refreshNext();
-  //   }, 10000);
-  // },
+  model(){
+    return this.store.queryRecord('scheduled-show', {next: true});
+  },
+  afterModel(){
+    this.refreshNext();
+  },
+  refreshNext() {
+    this.model();
+    Ember.run.later(() => {
+      this.refreshNext();
+    }, 10000);
+  },
 });
