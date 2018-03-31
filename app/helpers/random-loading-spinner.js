73a47b305b5dc73e74a97d162c1ca8c6ca9471cd
Tony Miller <mcfiredrill@gmail.com>
2018-03-31 13:21:18 +0900

    add datacoin spinner

--- app/helpers/random-loading-spinner.js
+++ app/helpers/random-loading-spinner.js
@@ -3,7 +3,8 @@ import { helper } from '@ember/component/helper';
 export function randomLoadingSpinner() {
   let loadingSpinners = [
     "/assets/images/jambox_spin.gif",
-    "/assets/images/anotherpizza_rotate.gif"
+    "/assets/images/anotherpizza_rotate.gif",
+    "/assets/images/datacoin.gif"
   ];
   return loadingSpinners[Math.floor(Math.random()*loadingSpinners.length)];
 }
