eefb36258e39c8d8107b963ae2bebf714aa64c88
Tony Miller <mcfiredrill@gmail.com>
2018-04-06 18:46:47 +0900

    WIP use snailzone server for visuals

--- app/components/datafruits-visuals.js
+++ app/components/datafruits-visuals.js
@@ -1,91 +1,113 @@
 /* global $, gapi */
-import $ from 'jquery';
+//import $ from 'jquery';
 import Component from '@ember/component';
-import { later } from '@ember/runloop';
+//import { later } from '@ember/runloop';
 import ENV from "datafruits13/config/environment";
 
 export default Component.extend({
-  // googleApiKey: "AIzaSyA2tCCcRl5itJoSLRL-COoHXwpyMAX9raQ",
-  // youtubeChannelId: "UChjo2k-w5UhvroZU0xqVcsg",
-  googleApiKey: ENV.GOOGLE_API_KEY,
-  youtubeChannelId: ENV.YOUTUBE_CHANNEL_ID,
-  ytid: "",
-  playerVars: {
-    autoplay: 1,
-    mute: 1,
-    volume: 0,
-    controls: 0,
-    enablejsapi: 1,
-    rel: 0, // disable related videos
-    showinfo: 0,
-    autohide: 1,
-    fs: 0, // disable fullscreen button
-    playsinline: 1,
-    disablekb: 1,
-    // iv_load_policy: 3,
-    // modestbranding: 1,
-  },
-
   classNames: ['visuals'],
-  pollYoutubeApi: function(){
+  // googleApiKey: ENV.GOOGLE_API_KEY,
+  // youtubeChannelId: ENV.YOUTUBE_CHANNEL_ID,
+  streamHost: ENV.STREAM_HOST,
+  streamName: ENV.STREAM_NAME,
+
+  autoplay: false,
+
+  isMobile() {
+    if( navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)
+    ){
+
+      return true;
+    } else {
+      return false;
+    }
   },
-  pollVjApi: function(){
-    var _this = this;
-    later(function() {
-      _this.setVisuals();
-      _this.pollVjApi();
-    }, 10000);
-  },
-  setVisuals: function(){
-    var url = "https://datafruits.streampusher.com/vj/enabled.json";
-    $.get(url, function(data){
-      var vj_enabled = data.vj_enabled;
-      if(vj_enabled === true){
-        $(".visuals").show();
-      }else{
-        $(".visuals").hide();
-      }
-    });
-  },
-  searchYoutube: function(){
-    var channelId = this.youtubeChannelId;
-    var request = gapi.client.youtube.search.list({
-      part: 'snippet',
-      channelId: channelId,
-      maxResults: 1,
-      type: 'video',
-      eventType: 'live'
+
+  initializePlayer(name, extension) {
+    let type;
+    let liveStream = false;
+    if (extension == "mp4") {
+      type = "video/mp4";
+      liveStream = false;
+    }
+    else if (extension == "m3u8") {
+      type = "application/x-mpegURL";
+      liveStream = true;
+    }
+    else {
+      console.log("Unknown extension: " + extension);
+      return;
+    }
+
+    let preview = name;
+    if (name.endsWith("_adaptive")) {
+      preview = name.substring(0, name.indexOf("_adaptive"));
+    }
+
+    let player = videojs('video-player', {
+      poster: `previews/${preview}.png`
 
     });
 
-    request.then((response) => {
-      if(response.result.pageInfo.totalResults === 0){
-        console.log("no live stream at the moment");
-      }else{
-        var videoId = response.result.items[0].id.videoId;
-        console.log("live stream: "+videoId);
-        this.set("ytid", videoId);
-      }
-    }, function(reason) {
-      console.log('Error: ' + reason.result.error.message);
+    player.src({
+      src: `streams/${name}.${extension}`,
+      type: type
     });
 
+
+    if (this.get('autoPlay')) {
+
+      player.play();
+
+    }
+
   },
+
+  //ask if adaptive m3u8 file
+  //
   didInsertElement(){
-    this.setVisuals();
-    this.pollVjApi();
-    $.getScript("https://apis.google.com/js/client.js", () => {
-      // Keep checking until the library is loaded
-      var googleApiKey = this.googleApiKey;
-      var searchYoutube = this.searchYoutube.bind(this);
-      (function checkIfLoaded() {
-        if (gapi.client){
-          gapi.client.setApiKey(googleApiKey);
-          gapi.client.load('youtube', 'v3').then(searchYoutube);
-        }else{
-          window.setTimeout(checkIfLoaded, 10);
-        }
-      })();
+    if(!this.isMobile()){
+      this.set('autoPlay', true);
+    }
+    let name = this.get('streamName');
+    let host = this.get('streamHost');
+    fetch(`${host}/streams/${name}_adaptive.m3u8`, {method:'HEAD'}).then(function(response) {
+      if (response.status == 200) {
+        //// adaptive m3u8 existslay it
+        this.initializePlayer(`${name}_adaptive`, "m3u8");
+      } else {
+        //adaptive m3u8 not exists, try m3u8 exists.
+        fetch(`${host}/streams/${name}.m3u8`, {method:'HEAD'}).then(function(response) {
+          if (response.status == 200) {
+            //m3u8 exists, play it
+            this.initializePlayer(name, "m3u8");
+          } else {
+            //no m3u8 exists, try vod file
+
+            fetch(`${host}/streams/${name}.mp4`, {method:'HEAD'}).then(function(response) {
+              if (response.status == 200) {
+                //mp4 exists, play it
+                this.initializePlayer(name, "mp4");
+              } else {
+                    console.log("No stream found");
+              }
+            }).catch(function(err) {
+              console.log("Error: " + err);
+            });
+          }
+        }).catch(function(err) {
+          console.log("Error: " + err);
+        });
+      }
+
+    }).catch(function(err) {
+      console.log("Error: " + err);
     });
   }
 });
