6c1d6580c72de00159866a868d2e8e09ff16bf4d
Tony Miller <mcfiredrill@gmail.com>
2018-06-11 14:36:15 +0900

    add korean

--- app/locales/kr/translations.js
+++ app/locales/kr/translations.js
@@ -8,4 +8,13 @@ export default {
   // },
   //
   // "key.with.interpolation": "Text with {{anInterpolation}}"
+  'chat': '잡담',
+  'podcasts': '포드 캐스트',
+  'timetable': '시간표',
+  'label': '상표',
+  'about': '',
+  'blog': '블로그',
+  'shop': '가게',
+  'subscribe': '구독하다',
+  'djinquiry': 'DJ'
 };
