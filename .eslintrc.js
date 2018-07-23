7e232bd665479265e7aa104c9f6127de59366afc
Tony Miller <mcfiredrill@gmail.com>
2018-07-23 17:39:25 +0900

    message

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,8 +26,9 @@ module.exports = {
     // node files
     {
       files: [
-        'testem.js',
         'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
@@ -38,15 +39,6 @@ module.exports = {
       env: {
         browser: false,
         node: true
-      }
-    },
-
-    // test files
-    {
-      files: ['tests/**/*.js'],
-      excludedFiles: ['tests/dummy/**/*.js'],
-      env: {
-        embertest: true
       }
     }
   ]
