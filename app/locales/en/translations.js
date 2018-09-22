0c14f73539b2547dbe6d705088764ed67e4ad26d
Tony Miller <mcfiredrill@gmail.com>
2018-09-22 19:17:28 +0900

    thanks message

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -25,7 +25,8 @@ export default {
     'link_to_mix': "Link to a 1-2 hour mix you've made",
     'interval': "Would you like to do a daily, weekly, bi-weekly or monthly show?",
     "time": "what time would you like to do a show? typical shows are 2 hours. e.g. sundays at 3pm-5pm PST",
-    "error": "There was an error sending your application! Please check the error messages and try again."
+    "error": "There was an error sending your application! Please check the error messages and try again.",
+    "thanks": "Thanks for your application! We'll reply to your email as soon as possible."
   },
   'about_datafruits': "Datafruits is a netradio and netlabel founded in 2012 by Tony Miller (mcfiredrill). Its mission is to bring you the world's strangest electronic sounds and bring together the electronic music communities around the world.",
   'code_of_conduct': 'Code Of Conduct',
