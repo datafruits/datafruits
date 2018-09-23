a7734faa6711c831f61d19dd7074f0e3319cc31d
Tony Miller <mcfiredrill@gmail.com>
2018-09-23 18:37:48 +0900

    use init

--- app/controllers/dj-inquiry.js
+++ app/controllers/dj-inquiry.js
@@ -2,7 +2,10 @@ import Controller from '@ember/controller';
 //import computed from '@ember/object';
 
 export default Controller.extend({
-  intervals: ["daily", "weekly", "biweekly", "monthly", "other"],
+  init() {
+    this._super(...arguments);
+    this.set('intervals', ["daily", "weekly", "biweekly", "monthly", "other"]);
+  },
   actions: {
     setInterval(event){
       const interval = event.target.value;
@@ -11,13 +14,7 @@ export default Controller.extend({
     submit(){
       let hostApplication = this.get('model');
       hostApplication.set('timeZone', jstz.determine().name());
-      const success = () => {
-        console.log('success!');
-      };
-      const failure= () => {
-        console.log('failed!');
-      };
-      hostApplication.save().then(success, failure);
+      hostApplication.save();
     }
   }
 });
