fbfb29b2fb83e787368cea9b5aad97efcf11b3f5
Tony Miller <mcfiredrill@gmail.com>
2018-09-28 09:36:50 +0900

    fix mixed tabs and spaces

--- app/routes/podcasts.js
+++ app/routes/podcasts.js
@@ -9,12 +9,12 @@ export default Route.extend({
     }
   },
   model(params){
-	  params.page = params.page || 1;
-      return this.store.queryRecord('podcast', { name: 'datafruits', page: params.page }).then((podcast) => {
-    	return hash({
-		  tracks: podcast.get('tracks'),
-		  meta: App.storeMeta['podcast'],
-      	  labels: this.store.findAll('label'),
+    params.page = params.page || 1;
+    return this.store.queryRecord('podcast', { name: 'datafruits', page: params.page }).then((podcast) => {
+      return hash({
+        tracks: podcast.get('tracks'),
+        meta: App.storeMeta['podcast'],
+        labels: this.store.findAll('label'),
       })
     });
   }
