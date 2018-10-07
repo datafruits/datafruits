15805c7fb737d385ad9dbdccc1557acdec0e187f
Tony Miller <mcfiredrill@gmail.com>
2018-10-07 23:44:42 +0900

    cleanup

--- app/controllers/timetable.js
+++ app/controllers/timetable.js
@@ -1,18 +1,4 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  actions: {
-    showEvent: function(show){
-      this.transitionTo('show', show);
-    },
-
-    calendarAddOccurrence: function(occurrence) {
-    },
-
-    calendarUpdateOccurrence: function(occurrence, properties, isPreview) {
-    },
-
-    calendarRemoveOccurrence: function(occurrence) {
-    }
-  }
 });
