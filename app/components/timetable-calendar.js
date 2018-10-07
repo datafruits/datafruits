1a1395861c30b330661d1754e236dd43a3c8e149
Tony Miller <mcfiredrill@gmail.com>
2018-10-07 13:49:27 +0900

    remove unused code from timetable-calendar

--- app/components/timetable-calendar.js
+++ app/components/timetable-calendar.js
@@ -1,12 +1,4 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  eventSources: [
-    {
-      url: "https://datafruits.streampusher.com/scheduled_shows.json",
-      data: {
-        fullcalendar: true
-      }
-    }
-  ],
 });
