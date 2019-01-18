3cbb9adad1f40a42e61f741fc54ed0b125b5e413
Tony Miller <mcfiredrill@gmail.com>
2019-01-17 22:35:46 -0500

    fetch shows 1 month ahead/before

--- app/components/timetable-loader.js
+++ app/components/timetable-loader.js
@@ -17,6 +17,7 @@ export default Component.extend({
   },
 
   fetchData: task(function*(query) {
+    // FIXME document not available in fastboot!
     if(document.getElementsByClassName("as-calendar-timetable__main").length){
       document.getElementsByClassName("as-calendar-timetable__main")[0]
         .classList.add('bleed')
@@ -25,11 +26,11 @@ export default Component.extend({
     query.timezone = jstz.determine().name();
     const start = query.start;
     if(query.view === 'month'){
-      query.start = moment(start).startOf('month').format('YYYY-MM-DD');
-      query.end = moment(start).endOf('month').format('YYYY-MM-DD');
+      query.start = moment(start).startOf('month').subtract(1, 'month').format('YYYY-MM-DD');
+      query.end = moment(start).endOf('month').add(1, 'month').format('YYYY-MM-DD');
     }else{
-      query.start = moment(start).startOf('week').format('YYYY-MM-DD');
-      query.end = moment(start).endOf('week').format('YYYY-MM-DD');
+      query.start = moment(start).startOf('week').subtract(1, 'week').format('YYYY-MM-DD');
+      query.end = moment(start).endOf('week').add(1, 'week').format('YYYY-MM-DD');
     }
     let shows = this.store.query('scheduled-show', query).then((shows) => {
       return shows;
