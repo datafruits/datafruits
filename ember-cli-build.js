41cd0981ca2617ee615a1d477ab9575cc3a11f9d
Tony Miller <mcfiredrill@gmail.com>
2019-02-10 12:30:36 +0900

    switch from jquery-textcomplete to textcomplete

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -43,12 +43,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  // if (typeof FastBoot === 'undefined') {
-  //   //app.import(app.bowerDirectory + '/jplayer/dist/jplayer/jquery.jplayer.js');
-  //   app.import(app.bowerDirectory + '/jquery-textcomplete/dist/jquery.textcomplete.min.js');
-  // }
-
-  app.import('/vendor/emojione_autocomplete.css');
 
   app.import('/vendor/Condiment-Regular.ttf');
 
