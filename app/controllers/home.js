f2c053819ff180ed9fb04c93f2ef2a879de13aec
Tony Miller <mcfiredrill@gmail.com>
2019-06-11 12:01:42 +0900

    remove bootstrap
    * redid the mobile menu
    * changed logo font to debussy

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,10 +4,26 @@ import { oneWay } from '@ember/object/computed';
 
 export default Controller.extend({
   i18n: service(),
+  router: service(),
   locale: oneWay('i18n.locale'),
+  menuOpen: false,
+  submenuOpen: false,
+  init(){
+    this._super(...arguments);
+    this.router.on('routeWillChange', () => {
+      this.set('menuOpen', false);
+      this.set('submenuOpen', false);
+    });
+  },
   actions: {
     setLocale(locale){
       this.set('i18n.locale', locale);
+    },
+    toggleMenu(){
+      this.toggleProperty('menuOpen');
+    },
+    toggleSubMenu(){
+      this.toggleProperty('submenuOpen');
     }
   }
 });
