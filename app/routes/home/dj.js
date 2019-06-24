40e887e2b660dedf36a554a8169243d99b2d0dae
Tony Miller <mcfiredrill@gmail.com>
2019-06-25 01:45:45 +0900

    set head tags for dj route

--- app/routes/home/dj.js
+++ app/routes/home/dj.js
@@ -1,10 +1,43 @@
 import Route from '@ember/routing/route';
+import ENV from 'datafruits13/config/environment';
 
 export default Route.extend({
   model(params) {
     return this.store.queryRecord('dj', { name: params.name });
 
   },
+  afterModel(model) {
+   this.setHeadTags(model);
+  },
+
+  setHeadTags(model) {
+   const headTags = ENV.headTags.concat([
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:title',
+         content: `datafruits.fm - ${model.username}`
+       },
+     },
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:description',
+         content: model.bio
+       },
+     },
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:image',
+         content: model.imageUrl
+       },
+     },
+   ]);
+
+   this.set('headTags', headTags);
+  },
+
   serialize(dj) {
     return {
       name: dj.get('name')
