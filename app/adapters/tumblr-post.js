8aedcdffbc9f391dd53b7d1e9576fe3f7f4e9b59
Tony Miller <mcfiredrill@gmail.com>
2019-07-15 00:06:09 +0900

    new frontpage

--- app/adapters/tumblr-post.js
+++
@@ -1,7 +0,0 @@
-// adapters/tumblr-post.js
-import PostAdapter from 'ember-tumblr/adapters/tumblr-post';
-
-export default PostAdapter.extend({
-  blogUrl: 'datafruits.tumblr.com',
-  apiKey: 'l2LGUWExJu5OIVAei9AYib8smwRwuJ3o8CsHz2aaIznqrioC8w'
-});
