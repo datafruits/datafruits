0277e0507f501c5c1d6130f1348125cba9fbe371
Tony Miller <mcfiredrill@gmail.com>
2019-10-28 19:05:05 +0900

    set vh property on initial insert

--- app/components/window-resize-handler.js
+++ app/components/window-resize-handler.js
@@ -3,9 +3,9 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: '',
   didInsertElement(){
-    // We listen to the resize event
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     window.addEventListener('resize', () => {
-      // We execute the same script as before
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
