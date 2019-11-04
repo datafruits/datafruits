a78cfee4cc11543ac67e4a5dd4a37f0ce133e27a
Tony Miller <mcfiredrill@gmail.com>
2019-11-04 19:36:14 +0900

    fix datafruits-chat test

--- tests/integration/components/datafruits-chat-test.js
+++ tests/integration/components/datafruits-chat-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | datafruits chat', function(hooks) {
@@ -10,13 +10,15 @@ module('Integration | Component | datafruits chat', function(hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
 
-    await render(hbs`{{datafruits-chat}}`);
+    await render(hbs`<DatafruitsChat />`);
 
     assert.dom('*').hasText('Join chatroom');
   });
 
   test('it has offline message', async function(assert) {
-    await render(hbs`{{datafruits-chat}}`);
+    this.set('networkStatus', {});
+    this.set('networkStatus.isOffline', true);
+    await render(hbs`<DatafruitsChat @networkStatus={{this.networkStatus}} />`);
 
     assert.dom('*').hasText("You appear to be offline. I guess you won't be able to chat. Musta been the onion salad dressing Join chatroom");
   });
