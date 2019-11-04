57dbdd89aeae2ec4144d0149530121eef8a93ad3
Tony Miller <mcfiredrill@gmail.com>
2019-11-04 19:03:53 +0900

    add offline message test to datafruits-chat-test

--- tests/integration/components/datafruits-chat-test.js
+++ tests/integration/components/datafruits-chat-test.js
@@ -7,13 +7,17 @@ module('Integration | Component | datafruits chat', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    assert.expect(1);
-
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
 
     await render(hbs`{{datafruits-chat}}`);
 
     assert.dom('*').hasText('Join chatroom');
+  });
+
+  test('it has offline message', async function(assert) {
+    await render(hbs`{{datafruits-chat}}`);
+
+    assert.dom('*').hasText("You appear to be offline. I guess you won't be able to chat. Musta been the onion salad dressing Join chatroom");
   });
 });
