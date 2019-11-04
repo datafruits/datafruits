c7e77e89defc999223954debb6d7ed72c8423a9d
Tony Miller <mcfiredrill@gmail.com>
2019-11-04 19:43:33 +0900

    stub chat service in datafruits-chat-test

--- tests/integration/components/datafruits-chat-test.js
+++ tests/integration/components/datafruits-chat-test.js
@@ -2,9 +2,18 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import Service from '@ember/service';
+
+const chatStub = Service.extend({
+});
 
 module('Integration | Component | datafruits chat', function(hooks) {
   setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.owner.register('service:chat', chatStub);
+  });
+
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
