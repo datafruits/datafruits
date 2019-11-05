742b27ec134634461eb529fd95544d5115c8175d
Tony Miller <mcfiredrill@gmail.com>
2019-11-05 15:47:48 +0900

    remove console logs

--- app/components/chat-lazy-image.js
+++ app/components/chat-lazy-image.js
@@ -4,9 +4,7 @@ import { observer } from '@ember/object';
 
 export default LazyImage.extend({
   adjustScrollingIfImageLoaded: observer('loaded', function(){
-    console.log('in adjustScrollingIfImageLoaded');
     if(this.loaded === true){
-      console.log('calling adjustScrolling');
       this.adjustScrolling();
     }
   })
