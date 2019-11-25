e4e7d530f25abf3aa37f1f9e299fb479f75a9b86
Tony Miller <mcfiredrill@gmail.com>
2019-11-25 19:44:15 +0900

    swap rollbar for bugsnag

---
+++ app/initializers/bugsnag.js
@@ -0,0 +1,18 @@
+import ENV from "datafruits13/config/environment";
+
+export async function initialize(/* application */) {
+  if (typeof FastBoot === 'undefined') {
+    const bugsnagModule = await import('@bugsnag/js');
+
+    const bugsnagClient = bugsnagModule.bugsnag(ENV.BUGSNAG_KEY);
+    if (bugsnagClient) {
+      Ember.onerror = function (error) {
+        bugsnagClient.notify(error)
+      };
+    }
+  }
+}
+
+export default {
+  initialize
+};
