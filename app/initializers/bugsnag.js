f04ced40f57f8bedafc32c961094b7edaea50524
Tony Miller <mcfiredrill@gmail.com>
2019-11-26 15:21:11 +0900

    fix bugsnag initializer

--- app/initializers/bugsnag.js
+++ app/initializers/bugsnag.js
@@ -4,7 +4,7 @@ export async function initialize(/* application */) {
   if (typeof FastBoot === 'undefined') {
     const bugsnagModule = await import('@bugsnag/js');
 
-    const bugsnagClient = bugsnagModule.bugsnag(ENV.BUGSNAG_KEY);
+    const bugsnagClient = bugsnagModule.default(ENV.BUGSNAG_KEY);
     if (bugsnagClient) {
       Ember.onerror = function (error) {
         bugsnagClient.notify(error)
