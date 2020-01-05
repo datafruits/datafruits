1e5d784395e97a0f167538d29d1b59979a2d2c59
Tony Miller <mcfiredrill@gmail.com>
2020-01-06 02:09:31 +0900

    setup mirage

--- tests/integration/components/podcasts-search-test.js
+++ tests/integration/components/podcasts-search-test.js
@@ -2,9 +2,11 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | podcasts search', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
   test('it renders', async function(assert) {
 
