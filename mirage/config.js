1adfc17c51bf1cf344c8bba01b0c48705d593da1
Tony Miller <mcfiredrill@gmail.com>
2020-01-11 12:29:33 +0900

    setup mirage/config

--- mirage/config.js
+++ mirage/config.js
@@ -1,3 +1,5 @@
+import ENV from "datafruits13/config/environment";
+
 export default function() {
 
   // These comments are here to help you get started. Feel free to delete them.
@@ -9,6 +11,7 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.urlPrefix = ENV.API_HOST;    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
@@ -23,4 +26,6 @@ export default function() {
 
     https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
   */
+
+  this.get('/podcasts/datafruits.json');
 }
