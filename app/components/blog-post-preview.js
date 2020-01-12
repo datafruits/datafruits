560d1942868e68f28089ab227e76a6343d7344ba
Tony Miller <mcfiredrill@gmail.com>
2020-01-13 07:34:19 +0900

    check if blog post body exists in preview component

--- app/components/blog-post-preview.js
+++ app/components/blog-post-preview.js
@@ -4,8 +4,10 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   i18n: service(),
-  previewImage: computed('body.blogPostImages', function(){
-    return this.body.blogPostImages.firstObject;
+  previewImage: computed('body.blogPostImages.[]', function(){
+    if(this.body) {
+      return this.body.blogPostImages.firstObject;
+    }
   }),
   body: computed('i18n.locale', function(){
     let body = this.post.blogPostBodies.filter( (body) => {
