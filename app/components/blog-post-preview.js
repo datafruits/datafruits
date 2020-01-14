407a7d822e632c81701c991f4139bc9dc53e80cf
Tony Miller <mcfiredrill@gmail.com>
2020-01-14 21:50:41 +0900

    set meta tags for blog posts

--- app/components/blog-post-preview.js
+++ app/components/blog-post-preview.js
@@ -4,11 +4,6 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   i18n: service(),
-  previewImage: computed('body.blogPostImages.[]', function(){
-    if(this.body) {
-      return this.body.blogPostImages.firstObject;
-    }
-  }),
   body: computed('i18n.locale', function(){
     let body = this.post.blogPostBodies.filter( (body) => {
       return body.language == this.i18n.locale;
--- app/models/blog-post-body.js
+++ app/models/blog-post-body.js
@@ -9,6 +9,11 @@ export default Model.extend({
   blogPostImages: DS.hasMany('blog-post-image'),
   title: DS.attr(),
   body: DS.attr(),
+  previewImage: computed('blogPostImages.[]', function(){
+    if(this.body) {
+      return this.blogPostImages.firstObject;
+    }
+  }),
   renderedBody: DS.attr(),
   htmlSafeBody: computed('renderedBody', function() {
     return htmlSafe(this.renderedBody);
