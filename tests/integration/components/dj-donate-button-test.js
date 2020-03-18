cf6538dd13c4116675e3f20b867b1aa7bdff4e23
Tony Miller <mcfiredrill@gmail.com>
2020-03-18 13:25:31 +0900

    fix dj-donate-button-test

--- tests/integration/components/dj-donate-button-test.js
+++ tests/integration/components/dj-donate-button-test.js
@@ -12,15 +12,6 @@ module('Integration | Component | dj-donate-button', function(hooks) {
 
     await render(hbs`<DjDonateButton />`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <DjDonateButton>
-        template block text
-      </DjDonateButton>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), 'DONATE');
   });
 });
