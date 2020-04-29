2c303b25debebaee42e390a5a29ead004118209a
Tony Miller <mcfiredrill@gmail.com>
2020-04-29 14:10:26 -0700

    try to use decorators for timetable

--- app/controllers/home/timetable.js
+++ app/controllers/home/timetable.js
@@ -1,35 +1,27 @@
-import classic from 'ember-classic-decorator';
-import { action, computed } from '@ember/object';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import Controller from '@ember/controller';
-import QueryParams from 'ember-parachute';
+import { queryParam } from 'ember-parachute/decorators';
 import moment from 'moment';
 
-export const TimetableQueryParams = new QueryParams({
-  start: {
-    defaultValue: moment().format('YYYY-MM-DD'),
-    refresh: true
-  },
-  view: {
-    defaultValue: 'week',
-    refresh: true
-  }
-});
+export default class TimetableController extends Controller {
+  @queryParam({ refresh: true}) start = moment().format('YYYY-MM-DD');
+  @queryParam({ refresh: true}) view = 'week';
 
-@classic
-export default class TimetableController extends Controller.extend(TimetableQueryParams.Mixin) {
-  @computed('start', 'view')
+  // @tracked start;
+  // @tracked view;
   get query() {
     return { start: this.start, view: this.view };
   }
 
   @action
   reloadCalendar(params) {
-    this.set('start', params.start);
-    this.set('view', params.view);
+    this.start = params.start;
+    this.view = params.view;
   }
 
   @action
   calendarTypeChange(type) {
-    this.set('view', type);
+    this.view = type;
   }
 }
