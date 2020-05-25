af6bdb206868ea5b84519a0a981ab9084d1145ff
Dakota Clark <dakotamusic@gmail.com>
2020-05-25 01:45:56 -0400

    improve styling, reduce container width

--- app/services/chat-text.js
+++ app/services/chat-text.js
@@ -2,7 +2,7 @@ import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class ChatTextService extends Service {
-  @tracked color = "color: yellow"
+  @tracked color = "color: #fff940"
 
   setColor(hexCode) {
     this.set('color', "color: " + hexCode)
