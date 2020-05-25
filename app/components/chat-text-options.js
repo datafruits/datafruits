25fa1ec170d557065f2b731894d42c1dbd84aea1
Dakota Clark <dakotamusic@gmail.com>
2020-05-25 01:34:00 -0400

    change label, and change its color with selection

--- app/components/chat-text-options.js
+++ app/components/chat-text-options.js
@@ -1,13 +1,16 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class ColorTextSelector extends Component {
+  @tracked style = "color: yellow"
   @service
   chatText
 
   @action
   selectedColor(event) {
+    this.style = "color: " + event.target.value
     this.chatText.setColor(event.target.value)
   }
 }
