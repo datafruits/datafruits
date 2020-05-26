26f6e3a81e8cd027590c7f891ce8a29de479ecd1
Dakota Clark <dakotamusic@gmail.com>
2020-05-25 23:31:37 -0400

    update input handler syntax and add semicolons

--- app/components/chat-text-options.js
+++ app/components/chat-text-options.js
@@ -4,13 +4,14 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class ColorTextSelector extends Component {
-  @tracked style = "color: yellow"
-  @service
-  chatText
+  @tracked style = "color: yellow";
+
+  @service 
+  chatText;
 
   @action
   selectedColor(event) {
-    this.style = "color: " + event.target.value
-    this.chatText.setColor(event.target.value)
+    this.style = "color: " + event.target.value;
+    this.chatText.setColor(event.target.value);
   }
 }
