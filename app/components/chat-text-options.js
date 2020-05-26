1125b6ad05ecd5b756ed1e1fc52a655a3db0fa5e
Dakota Clark <dakotamusic@gmail.com>
2020-05-25 23:41:34 -0400

    use oneWay instead of tracked for updating label color

--- app/components/chat-text-options.js
+++ app/components/chat-text-options.js
@@ -1,17 +1,16 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
+import { oneWay } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default class ColorTextSelector extends Component {
-  @tracked style = "color: yellow";
+  @oneWay('chatText.color') style;
 
   @service 
   chatText;
 
   @action
   selectedColor(event) {
-    this.style = "color: " + event.target.value;
     this.chatText.setColor(event.target.value);
   }
 }
