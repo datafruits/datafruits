89df17c0e23376e3ea36868f6d662637c76deabb
Tony Miller <mcfiredrill@gmail.com>
2020-06-29 09:51:49 -0700

    remove unused actions in gif-preview component

--- app/components/gif-preview.js
+++ app/components/gif-preview.js
@@ -1,18 +1,4 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class GifPreviewComponent extends Component {
-  @tracked previewUrl = this.args.gif.previewUrl;
-  @action
-  toggleBigGif() {
-    this.previewUrl = this.args.gif.url;
-    console.log('big');
-  }
-
-  @action
-  toggleSmallGif() {
-    this.previewUrl = this.args.gif.previewUrl;
-    console.log('small');
-  }
 }
