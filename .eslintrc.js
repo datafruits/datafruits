5b6e215acb5665488c1797f79647af213b29cf9f
Cassandra Cool <hey@cass.love>
2020-06-28 23:15:29 -0400

    Add prettier
    Integrated prettier with eslint to enforce commit code styling.

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended'
   ],
   env: {
     browser: true
