5f423c84175bff0891037315aaa79e0a01e428e7
Cassandra Cool <hey@cass.love>
2020-07-01 15:52:30 -0400

    Add arbitrarily large file to demonstrate bundle size differences

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
-import Application from '@ember/application';
-import Resolver from 'ember-resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import Application from "@ember/application";
+import Resolver from "ember-resolver";
+import loadInitializers from "ember-load-initializers";
+import config from "./config/environment";
+import large from "./large";
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+  _dummy = large;
 }
 
 loadInitializers(App, config.modulePrefix);
