e5d2892b588493154ffce2bb7db7beaeb2a998f7
Tony Miller <mcfiredrill@gmail.com>
2020-07-26 14:28:31 -0700

    convert nav components to glimmer components
    add custom df colors for tailwind

--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -2,14 +2,20 @@
 module.exports = {
   theme: {
     fontFamily: {
-      'debussy': ['debussy', 'sans-serif'],
+      debussy: ['debussy', 'sans-serif'],
     },
     extend: {
       height: {
         'handle-resize': 'calc(var(--vh, 1vh) * 100)',
       },
-    }
+      colors: {
+        'df-pink': '#e992ff',
+        'df-yellow': '#fff94f',
+        'df-blue': '#00b4ff',
+        'df-green': '#5fdc82',
+      },
+    },
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
