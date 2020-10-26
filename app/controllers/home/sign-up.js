7ef43e34d9d7b1a06f079222b93141104030a237
Tony Miller <mcfiredrill@gmail.com>
2020-10-26 23:18:01 +0900

    import validations in sign up controller

--- app/controllers/home/sign-up.js
+++ app/controllers/home/sign-up.js
@@ -1,3 +1,6 @@
 import Controller from '@ember/controller';
+import UserValidations from '../../validations/user';
 
-export default class HomeSignUpController extends Controller {}
+export default class HomeSignUpController extends Controller {
+  UserValidations = UserValidations;
+}
