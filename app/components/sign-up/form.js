71e38b9eca42645a9673d82bdcad21258c33de31
Tony Miller <mcfiredrill@gmail.com>
2020-10-27 12:32:41 +0900

    use or for disabling save button condition

--- app/components/sign-up/form.js
+++ app/components/sign-up/form.js
@@ -5,11 +5,6 @@ import { tracked } from '@glimmer/tracking';
 export default class SignUpFormComponent extends Component {
   @tracked cocAccepted = false;
 
-  get canSave() {
-    const changeset = this.args.changeset;
-    return !changeset.isSaving && changeset.isValid && this.cocAccepted === true;
-  }
-
   @action
   submit() {
     let user = this.model;
