2a3bea2c96dbb274509f4f762d5e64ed704a859b
Tony Miller <mcfiredrill@gmail.com>
2020-10-29 23:28:31 +0900

    add acceptance test for sign up

---
+++ tests/acceptance/sign-up-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | sign up', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /sign-up', async function(assert) {
+    await visit('/sign-up');
+
+    assert.equal(currentURL(), '/sign-up');
+
+    await fillIn("input#username", "djnameko");
+    await fillIn("input#email", "dj.nameko@datafruits.fm");
+    await fillIn("input#password", "mypassword1234");
+    await fillIn("input#password-confirmation", "mypassword1234");
+    await click("[data-test-submit-button]");
+
+    assert.equal(currentURL(), '/users/settings');
+  });
+});
