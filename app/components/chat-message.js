302f0d2d29309ae92aef19426aaa43ab801561c2
4lp <alptraq@gmail.com>
2020-11-15 09:47:25 -0600

    Add dj link from chat

--- app/components/chat-message.js
+++ app/components/chat-message.js
@@ -7,6 +7,10 @@ export default class ChatMessage extends Component {
   gifsEnabled = true;
   imgRegex = /https?:\/\/(?:[a-z0-9-]+\.)+[a-z]{2,6}(?:\/[^/#?]+)+\.(?:jpg|gif|png|webp)(\?.*$)*/;
 
+  get isDj() {
+    return this.message.role.includes("dj");
+  }
+  
   @computed('message.body', 'imgRegex')
   get hasImage() {
     return this.imgRegex.test(this.message.body);
