b1cfe5743c999f36029e0cf65fb7e269cd86dc43
Dakota Clark <dakotamusic@gmail.com>
2020-11-29 01:18:48 -0500

    add assertion for setting cocAccepted to false

--- tests/unit/controllers/home/dj-inquiry-test.js
+++ tests/unit/controllers/home/dj-inquiry-test.js
@@ -9,4 +9,9 @@ module('Unit | Controller | home/dj inquiry', function (hooks) {
     let controller = this.owner.lookup('controller:home/dj-inquiry');
     assert.ok(controller);
   });
+
+  test('it renders', async function (assert) {
+    let controller = this.owner.lookup('controller:home/dj-inquiry');
+    assert.equal(controller.cocAccepted, false, 'cocAccepted equals false');
+  });
 });
