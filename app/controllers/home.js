01b5431c676246941466091915ab512a051868f9
Tony Miller <mcfiredrill@gmail.com>
2020-12-02 13:04:12 +0900

    store current theme in cookie

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -43,11 +43,11 @@ export default class HomeController extends Controller {
   setTheme(theme) {
     console.log(`setting global theme "${theme}" ...`);
     let element = document.getElementsByTagName('html')[0];
-    // get currentTheme from cookie??
-    // let currentTheme =
-    // element.classList.remove(currentTheme);
+    let currentTheme = `theme-${localStorage.getItem('datafruits-theme') || 'classic'}`;
+    element.classList.remove(currentTheme);
     let themeName = `theme-${theme}`;
     element.classList.add(themeName);
+    localStorage.setItem('datafruits-theme', theme);
   }
 
   @action
