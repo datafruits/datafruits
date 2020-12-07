f2d9ac258c53f8a677ee9afce9956933157a2673
Tony Miller <mcfiredrill@gmail.com>
2020-12-07 11:48:18 +0900

    add premium subscriber landing page

--- app/router.js
+++ app/router.js
@@ -22,6 +22,7 @@ Router.map(function () {
     this.route('user', function () {
       this.route('settings');
     });
+    this.route('premium');
   });
   this.route('container', function () {
     this.route('show', { path: '/shows/:id' });
