d19e0a9ce6f3dcb66c59fe7e0486f6432905d6f5
Tony Miller <mcfiredrill@gmail.com>
2020-12-21 11:07:52 +0900

    fix comment

--- app/components/timetable-calendar.js
+++ app/components/timetable-calendar.js
@@ -23,15 +23,8 @@ export default class TimetableCalendarComponent extends Component {
 
   @action
   async fetchShows(query) {
+    /* TODO support query string... */
     query.timezone = jstz.determine().name();
-    // const start = query.start;
-    // if (query.view === 'month') {
-    //   query.start = moment(start).startOf('month').subtract(1, 'month').format('YYYY-MM-DD');
-    //   query.end = moment(start).endOf('month').add(1, 'month').format('YYYY-MM-DD');
-    // } else {
-    //   query.start = moment(start).startOf('week').subtract(1, 'week').format('YYYY-MM-DD');
-    //   query.end = moment(start).endOf('week').add(1, 'week').format('YYYY-MM-DD');
-    // }
     query.start = moment(new Date()).startOf('day').toString();
     query.end = moment(query.start).endOf('month').add(1, 'month').format('YYYY-MM-DD');
     let showsPromise = this.store.query('scheduled-show', query).then((result) => {
