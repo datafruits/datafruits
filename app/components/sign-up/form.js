efb13cd72900738ec6dd03c5fa559aea348f4d2b
Tony Miller <mcfiredrill@gmail.com>
2020-12-30 18:04:43 +0900

    get signup form mostly working

--- app/components/sign-up/form.js
+++ app/components/sign-up/form.js
@@ -1,15 +1,65 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class SignUpFormComponent extends Component {
   @tracked cocAccepted = false;
 
+  @service
+  session;
+
+  @service
+  router;
+
+  @action
+  validateProperty(property) {
+    let changeset = this.args.changeset;
+    changeset.validate(property);
+    console.log(`validated property: ${property}`);
+    console.log(changeset.get('errors'));
+    return changeset.validate(property);
+  }
+
+  @action
+  updateProperty(property, value) {
+    let changeset = this.args.changeset;
+    changeset.set(property, value);
+  }
+
   @action
   submit() {
-    let user = this.model;
-    user.save().then(() => {
-      //login user then transition to chat ??
+    let changeset = this.args.changeset;
+    changeset.validate().then(() => {
+      console.log(changeset);
+      if (changeset.isValid) {
+        changeset
+          .save()
+          .then(() => {
+            //login user then transition to chat ??
+            let nick = changeset.get('username');
+            let pass = changeset.get('password');
+            return this.session
+              .authenticate('authenticator:devise', nick, pass)
+              .then(() => {
+                const token = this.session.data.authenticated.token;
+                this.chat.push('authorize_token', { user: nick, timestamp: Date.now(), token: token });
+                this.router.transitionTo('chat');
+                return true;
+              })
+              .catch((/*reason*/) => {
+                alert('Wrong password');
+                return false;
+              });
+          })
+          .catch((error) => {
+            alert('couldnt save user!');
+            console.log(error);
+          });
+      } else {
+        console.log('changeset invalid');
+        console.log(changeset.get('errors'));
+      }
     });
   }
 }
--- app/components/validated-field.js
+++ app/components/validated-field.js
@@ -2,17 +2,35 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 
 export default class ValidatedField extends Component {
-  @action
-  updateProperty(event) {
+  get hasErrors() {
+    let keys = this.args.changeset.get('errors').map((error) => {
+      return error.key;
+    });
+    return keys.includes(this.args.property);
+  }
+
+  get errors() {
     let changeset = this.args.changeset;
     let property = this.args.property;
-    changeset.set(property, event.target.value);
+    return changeset
+      .get('errors')
+      .filter((error) => {
+        return error.key === property;
+      })
+      .map((error) => {
+        return error.validation;
+      });
+  }
+
+  @action
+  updateProperty(event) {
+    let property = this.args.property;
+    this.args.updateProperty(property, event.target.value);
   }
 
   @action
   validateProperty() {
-    let changeset = this.args.changeset;
     let property = this.args.property;
-    return changeset.validate(property);
+    return this.args.validateProperty(property);
   }
 }
