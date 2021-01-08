e7e05180088bef22c2dc992f9ebd49f2a8f27d32
Tony Miller <mcfiredrill@gmail.com>
2021-01-08 16:09:22 +0900

    add fruit selector

--- app/components/fruit-tip.js
+++ app/components/fruit-tip.js
@@ -1,28 +1,51 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class FruitTipComponent extends Component {
+  @tracked showingFruitChoices = false;
   @service
   chat;
 
+  fruitTypes = [
+    {
+      name: 'strawberry',
+      image: '/assets/images/emojis/strawbur.gif',
+    },
+    {
+      name: 'orange',
+      image: '/assets/images/emojis/orangey.gif',
+    },
+    {
+      name: 'lemon',
+      image: '/assets/images/emojis/lemoner.gif',
+    },
+  ];
+
+  // strawberry, orange, lemon
   @action
-  fruitTip(event) {
+  fruitTip(event, fruitType) {
     console.log('tip fruit!');
     if (this.chat.token) {
       this.chat.push('new:fruit_tip', {
         user: this.chat.username,
-        fruit: 'strawberry',
+        fruit: fruitType,
         timestamp: Date.now(),
         token: this.chat.token,
       });
     } else {
       this.chat.push('new:fruit_tip', {
         user: this.chat.username,
-        fruit: 'strawberry',
+        fruit: fruitType,
         timestamp: Date.now(),
       });
     }
     event.preventDefault();
+  }
+
+  @action
+  showFruitChoices() {
+    this.showingFruitChoices = true;
   }
 }
