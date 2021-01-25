69c8f63e507f3b1939853699976d5a855b0fd099
Tony Miller <mcfiredrill@gmail.com>
2021-01-25 13:43:18 +0900

    clean up pixi code

--- app/components/pixi.js
+++ app/components/pixi.js
@@ -23,7 +23,6 @@ export default class PixiComponent extends Component {
   addFruitTip(event) {
     console.log(`got fruitTipped event...in pixi component ${event}`);
     if (this.app) {
-      //let sprite = PIXI.Sprite.from('/assets/images/strawberry.png');
       let animation;
       if(this.fruits.includes(event)){
         animation = event;
@@ -82,27 +81,9 @@ export default class PixiComponent extends Component {
     //Add the canvas that Pixi automatically created for you to the HTML document
     document.body.appendChild(this.app.view);
 
-    // Create background image
-    //const background = PIXI.Sprite.from('https://cdn.glitch.com/ea97b047-b141-4edc-8fee-cabd43a14644%2Fiphone_girl_3d_2.png0001.png?v=1590408055180');
-    // const texture = PIXI.Texture.from(
-    //   'https://cdn.glitch.com/ea97b047-b141-4edc-8fee-cabd43a14644%2Fiphone_girl_small.png?v=1590408472784',
-    // );
-    //
-    // this.background = new PIXI.TilingSprite(texture, this.app.screen.width, this.app.screen.height);
-    // this.app.stage.addChild(this.background);
-
     // move this into resize handler later
     this.app.renderer.resize(window.innerWidth, window.innerHeight);
 
-    // You can use the 'screen' property as the renderer visible
-    // area, this is more useful than view.width/height because
-    // it handles resolution
-    //background.position.set(0, 0);
-    // this.background.width = this.app.screen.width;
-    // this.background.height = this.app.screen.height;
-    // end resize handler
-    //
-    //
     this.app.stop();
 
     this.app.loader.add('strawberry', 'assets/images/sprites/strawberry.json');
@@ -110,20 +91,10 @@ export default class PixiComponent extends Component {
     this.app.loader.add('lemon', 'assets/images/sprites/lemon.json');
     this.app.loader.add('shader', 'assets/shaders/shader.frag');
     this.app.loader.load((loader, res) => {
-      // Create the new filter, arguments: (vertexShader, framentSource)
       this.filter = new PIXI.Filter(null, res.shader.data, {
         customUniform: 0.0,
       });
 
-      // === WARNING ===
-      // specify uniforms in filter constructor
-      // or set them BEFORE first use
-      // filter.uniforms.customUniform = 0.0
-
-      // Add the filter
-      //this.background.filters = [this.filter];
-      //text.filters = [filter];
-      //
       this.animations.strawberry = res.strawberry.spritesheet.animations['strawberry2_wiggle.png'];
       this.animations.orange = res.orange.spritesheet.animations['orange.png'];
       this.animations.lemon = res.lemon.spritesheet.animations['lemon.png'];
@@ -137,11 +108,6 @@ export default class PixiComponent extends Component {
         this.filter.uniforms.customUniform += delta;
 
         count += 0.02;
-        // this.background.tileScale.x = 2 + Math.sin(count);
-        // this.background.tileScale.y = 2 + Math.cos(count);
-        //
-        // this.background.tilePosition.x += 1;
-        // this.background.tilePosition.y += 1;
 
         this.sprites.forEach((sprite) => {
           sprite.x += Math.sin(count);
