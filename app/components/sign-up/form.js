9f3c0d1f23363bfc657dda618de968e5fc61bd87
Tony Miller <mcfiredrill@gmail.com>
2021-02-20 17:37:01 +0900

    make it nice

--- app/components/sign-up/form.js
+++ app/components/sign-up/form.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { debounce } from '@ember/runloop';
 
 export default class SignUpFormComponent extends Component {
   @tracked cocAccepted = false;
@@ -26,6 +27,35 @@ export default class SignUpFormComponent extends Component {
     } else {
       return false;
     }
+  }
+
+  get emailValidationErrors() {
+    let errors = this.args.changeset.errors.find((error) => {
+      return error.key === 'email';
+    });
+    if(errors) {
+      return errors.validation;
+    } else {
+      return null;
+    }
+  }
+
+  _updateEmail() {
+    if (this.email && this.email.length > 5) {
+      console.log('_updateEmail setting changeset email');
+      this.args.changeset.set('email', this.email);
+    }
+  }
+
+  get everythingLooksNice() {
+    return this.args.changeset.isValid && !this.args.changeset.isSaving && this.cocAccepted;
+  }
+
+  @action
+  updateEmail(event) {
+    this.email = event.target.value;
+    console.log(`updateEmail: ${this.email}`);
+    debounce(this, this._updateEmail, 500);
   }
 
   @action
--- app/validations/user.js
+++ app/validations/user.js
@@ -1,9 +1,9 @@
-import { validatePresence } from 'ember-changeset-validations/validators';
+import { validatePresence, validateFormat } from 'ember-changeset-validations/validators';
 import uniqueEmail from 'datafruits13/validators/unique-email';
 import uniqueUsername from 'datafruits13/validators/unique-username';
 
 export default {
   username: [validatePresence(true), uniqueUsername()],
   password: validatePresence(true),
-  email: [validatePresence(true), uniqueEmail()],
+  email: [validateFormat({ type: 'email' }), uniqueEmail()],
 };
