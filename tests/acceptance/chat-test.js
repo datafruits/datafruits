eabc0e5e966f708b771ab1847596c4f94df649eb
Tony Miller <mcfiredrill@gmail.com>
2021-02-20 22:42:25 +0900

    lint chat test

--- tests/acceptance/chat-test.js
+++ tests/acceptance/chat-test.js
@@ -2,10 +2,10 @@ import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
-module('Acceptance | chat', function(hooks) {
+module('Acceptance | chat', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /chat', async function(assert) {
+  test('visiting /chat', async function (assert) {
     await visit('/chat');
 
     assert.equal(currentURL(), '/chat');
