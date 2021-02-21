1f165b80eea897e580fcb6c4fa44379ca94ce441
Tony Miller <mcfiredrill@gmail.com>
2021-02-21 12:12:35 +0900

    fix linting

--- mirage/config.js
+++ mirage/config.js
@@ -1,7 +1,6 @@
-import ENV from "datafruits13/config/environment";
+import ENV from 'datafruits13/config/environment';
 
-export default function() {
-
+export default function () {
   // These comments are here to help you get started. Feel free to delete them.
 
   /*
@@ -11,7 +10,7 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  this.urlPrefix = ENV.API_HOST;    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.urlPrefix = ENV.API_HOST; // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
