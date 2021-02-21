49ab6bb4cd302e71a447193c7c64ae8960a29626
Tony Miller <mcfiredrill@gmail.com>
2021-02-21 12:01:43 +0900

    fix selectors for sign up test

--- tests/acceptance/sign-up-test.js
+++ tests/acceptance/sign-up-test.js
@@ -1,19 +1,20 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
-module('Acceptance | sign up', function(hooks) {
+module('Acceptance | sign up', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /sign-up', async function(assert) {
+  test('visiting /sign-up', async function (assert) {
     await visit('/sign-up');
 
     assert.equal(currentURL(), '/sign-up');
 
-    await fillIn("input#username", "djnameko");
-    await fillIn("input#email", "dj.nameko@datafruits.fm");
-    await fillIn("input#password", "mypassword1234");
-    await click("[data-test-submit-button]");
+    await fillIn('[data-test-username]', 'djnameko');
+    await fillIn('[data-test-email]', 'dj.nameko@datafruits.fm');
+    await fillIn('[data-test-password]', 'mypassword1234');
+    await click('[data-test-coc]');
+    await click('[data-test-submit]');
 
     assert.equal(currentURL(), '/chat');
   });
