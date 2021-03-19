eb4451de2cf9b741c37f430db2a78c09040a47cc
Tony Miller <mcfiredrill@gmail.com>
2021-03-19 14:10:25 +0900

    stub window.alert in test

--- tests/acceptance/user-settings-test.js
+++ tests/acceptance/user-settings-test.js
@@ -1,15 +1,35 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
+import { currentSession, authenticateSession, invalidateSession } from 'ember-simple-auth/test-support';
 
 module('Acceptance | user settings', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
   test('visiting /user-settings', async function (assert) {
+    window.alert = function (msg) {
+      console.log(msg);
+    };
+    await authenticateSession({
+      username: 'djnameko',
+      password: '12345',
+    });
+    await visit('/login');
+
     await visit('/user/settings');
 
+    // upload photo
+    // change style
+    await click('[data-test-submit]');
+
     assert.equal(currentURL(), '/user/settings');
+  });
+
+  test('redirects to chat if not logged in', async function (assert) {
+    await visit('/user/settings');
+
+    assert.equal(currentURL(), '/chat');
   });
 });
