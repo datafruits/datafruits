89dea0b9f7b4a0b62c8ded66add65c2ae4540054
Tony Miller <mcfiredrill@gmail.com>
2021-03-19 15:07:46 +0900

    fix controller test

--- tests/unit/controllers/home/authenticated/user/settings-test.js
+++ tests/unit/controllers/home/authenticated/user/settings-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Controller | home/user/settings', function (hooks) {
+module('Unit | Controller | home/authenticated/user/settings', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
-    let controller = this.owner.lookup('controller:home/user/settings');
+    let controller = this.owner.lookup('controller:home/authenticated/user/settings');
     assert.ok(controller);
   });
 });
