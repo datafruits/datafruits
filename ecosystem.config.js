0824ed288eea45fa76a0874536dde1b0882fa795
Tony Miller <mcfiredrill@gmail.com>
2021-03-20 15:21:01 +0900

    try using pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      script: 'fastboot_server/server.js',
+      watch: '.',
+    },
+  ],
+
+  deploy: {
+    production: {
+      'pre-deploy-local': '',
+      'post-deploy': 'yarn install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
+};
