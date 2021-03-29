6c2c6221c3a0b5c425c0453cdd4bb8003880068b
Tony Miller <mcfiredrill@gmail.com>
2021-03-29 18:39:16 +0900

    fix chat test

--- tests/acceptance/chat-test.js
+++ tests/acceptance/chat-test.js
@@ -1,11 +1,9 @@
 import { module, test, skip } from 'qunit';
-import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
+import { visit, currentURL, fillIn, click, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Acceptance | chat', function (hooks) {
   setupApplicationTest(hooks);
-  setupMirage(hooks);
 
   // TODO need to mock websockets request somehow...
   skip('visiting /chat', async function (assert) {
@@ -16,13 +14,14 @@ module('Acceptance | chat', function (hooks) {
 
   test('login to the chat', async function (assert) {
     await visit('/chat');
-    await click('Login');
+    await click('[data-test-login-button]');
 
     await fillIn('[data-test-username]', 'djnameko');
     await fillIn('[data-test-password]', 'mypassword1234');
 
-    await click('[data-test-submit]');
+    await click('[data-test-login-submit]');
 
     assert.equal(currentURL(), '/chat');
+    assert.equal(find('[data-test-chat-input-messsage]').length, 1);
   });
 });
