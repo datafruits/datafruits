8afa04b6510837c966cc815ab1a91a606ede209c
Tony Miller <mcfiredrill@gmail.com>
2021-04-18 22:40:56 +0900

    remove giphy loader component

--- app/components/giphy-search.js
+++ app/components/giphy-search.js
@@ -1,20 +1,18 @@
-import classic from 'ember-classic-decorator';
-import { observes } from '@ember-decorators/object';
-import Component from '@ember/component';
-import '@ember/object'; //eslint-disable-line ember/no-observers
-import { debounce } from '@ember/runloop';
+import Component from '@glimmer/component';
+//import { debounce } from '@ember/runloop';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-@classic
 export default class GiphySearch extends Component {
   @tracked searchTerm;
-  // TODO convert this to used tracked
-  @observes('searchTerm') //eslint-disable-line ember/no-observers
-  observeQuery() {
-    //eslint-disable-line ember/no-observers
-    debounce(this, this.setQuery, 500);
-  }
 
-  setQuery() {
-    this.set('query', this.searchTerm);
+  @service
+  store;
+
+  @action
+  loadGifs(){
+    //should debounce ???
+    return this.store.query('gif', { query: this.searchTerm });
   }
 }
