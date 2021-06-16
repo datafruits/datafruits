1c65adab6de64545fbd9aec813af173a345ce931
Tony Miller <mcfiredrill@gmail.com>
2021-06-16 15:46:26 +0900

    don't load current user in chat route

--- app/routes/home/chat.js
+++ app/routes/home/chat.js
@@ -5,33 +5,6 @@ import ENV from 'datafruits13/config/environment';
 export default class ChatRoute extends Route {
   @service session;
 
-  @service currentUser;
-
-  @service chat;
-
-  _loadCurrentUser() {
-    console.log('in _loadCurrentUser in chat');
-    if (this.session.isAuthenticated) {
-      console.log('session is authenticated');
-      return this.currentUser
-        .load()
-        .then(() => {
-          const nick = this.currentUser.user.username;
-          const avatarUrl = this.currentUser.user.avatarUrl;
-          const token = this.session.data.authenticated.token;
-          this.chat.push('authorize_token', { user: nick, timestamp: Date.now(), token: token, avatarUrl: avatarUrl });
-        })
-        .catch(() => {
-          console.log("couldn't load this.currentUser in chat _loadCurrentUser, invalidating session");
-          this.session.invalidate();
-        });
-    }
-  }
-
-  beforeModel() {
-    return this._loadCurrentUser();
-  }
-
   afterModel() {
     this.setHeadTags();
   }
