2ddd2b534eeb0ac89a9eba45869223af31e1bbd9
Tony Miller <mcfiredrill@gmail.com>
2021-06-17 09:45:55 +0900

    fix chat route

--- app/routes/home/chat.js
+++ app/routes/home/chat.js
@@ -2,10 +2,6 @@ import Route from '@ember/routing/route';
 import ENV from 'datafruits13/config/environment';
 
 export default class ChatRoute extends Route {
-  beforeModel() {
-    return this._loadCurrentUser();
-  }
-
   afterModel() {
     this.setHeadTags();
   }
