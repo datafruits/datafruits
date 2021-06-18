16e7183d9fce0e52dcf2ca9cea71f9d571f6a26b
Tony Miller <mcfiredrill@gmail.com>
2021-06-18 23:17:34 +0900

    fix mirage sign in endpoint

--- mirage/config.js
+++ mirage/config.js
@@ -11,6 +11,7 @@ export default function () {
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   this.urlPrefix = ENV.API_HOST; // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.logging = true;
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
@@ -75,7 +76,7 @@ export default function () {
   this.post('/users/sign_in', (schema, request) => {
     const attrs = JSON.parse(request.requestBody).user;
     const login = attrs.login;
-    return { login: login, redirect: '/', success: true, token: 'xxxxxxxxx', user_id: 26 };
+    return { login: login, redirect: '/', success: true, token: 'xxxxxxxxx', user_id: 26, test: 'hey' };
   });
 
   this.get('/users/current_user.json', (/* schema, request */) => {
