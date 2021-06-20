c5c89d00f5c5da2cfb01f653c73514caf2542991
Tony Miller <mcfiredrill@gmail.com>
2021-06-20 10:45:49 +0900

    fix hat test

--- tests/unit/routes/hat-test.js
+++ tests/unit/routes/hat-test.js
@@ -5,7 +5,7 @@ module('Unit | Route | hat', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:hat');
+    let route = this.owner.lookup('route:home/hat');
     assert.ok(route);
   });
 });
