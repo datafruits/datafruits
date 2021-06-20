d9b068012d9f5c2cb0010a996042cdab6d15dc4a
Tony Miller <mcfiredrill@gmail.com>
2021-06-20 11:40:59 +0900

    fix cat test

--- tests/unit/routes/cat-test.js
+++ tests/unit/routes/cat-test.js
@@ -5,7 +5,7 @@ module('Unit | Route | cat', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:cat');
+    let route = this.owner.lookup('route:home/cat');
     assert.ok(route);
   });
 });
