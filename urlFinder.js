a33c1f8e5e9319b6f44554b43bb70826c4c76f27
Tony Miller <mcfiredrill@gmail.com>
2021-07-09 12:15:32 +0900

    try to crawl all podcast URLs

--- urlFinder.js
+++ urlFinder.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-module.exports = async function({ distDir, visit }) {
+module.exports = async function ({ distDir, visit }) {
   let urls = [
-        '/',
-        '/timetable',
-        '/about',
-        '/subscribe',
-        '/dj-inquiry',
-        '/coc',
-        '/djs',
-        '/chat',
-        '/sign-up',
+    '/',
+    '/timetable',
+    '/podcasts',
+    '/about',
+    '/subscribe',
+    '/dj-inquiry',
+    '/coc',
+    '/djs',
+    '/chat',
+    '/sign-up',
   ];
 
   // need to recursively crawl all the links on every page somehow
@@ -23,14 +24,35 @@ module.exports = async function({ distDir, visit }) {
       let html = await page.html();
       let dom = new JSDOM(html);
       for (let aTag of [...dom.window.document.querySelectorAll('a')]) {
-        console.log(aTag.href);
         if (aTag.href) {
           console.log(aTag.hostname);
           console.log(aTag.host);
           // exclude external links
           //
-          if(!aTag.hostname && !urls.includes(aTag.pathname)) {
-            urls.push(aTag.pathname);
+          console.log(aTag.href);
+          if (!aTag.hostname && !urls.includes(aTag.href)) {
+            urls.push(aTag.href);
+          }
+        }
+      }
+      if (url === '/podcasts') {
+        for (let aTag of [...dom.window.document.querySelectorAll('span.pagination a')]) {
+          page = await visit(url);
+          if (page.statusCode === 200) {
+            let html = await page.html();
+            let dom = new JSDOM(html);
+            for (let aTag of [...dom.window.document.querySelectorAll('a')]) {
+              if (aTag.href) {
+                console.log(aTag.hostname);
+                console.log(aTag.host);
+                // exclude external links
+                //
+                console.log(aTag.href);
+                if (!aTag.hostname && !urls.includes(aTag.href)) {
+                  urls.push(aTag.href);
+                }
+              }
+            }
           }
         }
       }
@@ -38,4 +60,4 @@ module.exports = async function({ distDir, visit }) {
   }
 
   return urls;
-}
+};
