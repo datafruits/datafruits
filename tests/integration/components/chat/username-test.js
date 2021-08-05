71136f1ef687052fec7eba1044e29897bbf6f7dc
Tony Miller <mcfiredrill@gmail.com>
2021-08-04 17:55:54 -0700

    fix style

--- tests/integration/components/chat/username-test.js
+++ tests/integration/components/chat/username-test.js
@@ -3,24 +3,15 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | chat/username', function(hooks) {
+module('Integration | Component | chat/username', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<Chat::Username />`);
 
     assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Chat::Username>
-        template block text
-      </Chat::Username>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
