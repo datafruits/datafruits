dbb4cd031bb3c6697ab4a0533d32495953ae893f
Tony Miller <mcfiredrill@gmail.com>
2021-08-25 17:05:06 -0700

    remove ember-lazy-image, use native browser solution

--- app/components/chat-lazy-image.js
+++ app/components/chat-lazy-image.js
@@ -1,13 +1,23 @@
-import { observes } from '@ember-decorators/object';
-/* eslint ember/no-observers: 0 */
-import LazyImage from 'ember-lazy-image/components/lazy-image';
-import '@ember/object';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class ChatLazyImage extends LazyImage {
-  @observes('loaded')
+export default class ChatLazyImage extends Component {
   adjustScrollingIfImageLoaded() {
-    if (this.loaded === true) {
-      this.args.adjustScrolling();
+    this.args.adjustScrolling();
+  }
+
+  @action
+  didInsert(element) {
+    console.log('inserted image');
+    let img = element.querySelector('img');
+    console.log(img);
+    if (img.complete) {
+      this.adjustScrollingIfImageLoaded();
+    } else {
+      img.addEventListener('load', this.adjustScrollingIfImageLoaded);
+      img.addEventListener('error', function () {
+        alert('error');
+      });
     }
   }
 }
