5fa9dca9f04cf1a3250339f2e9e3e08a78fd8153
Tony Miller <mcfiredrill@gmail.com>
2021-09-23 10:47:03 -0700

    remove old studio phone message :'(

--- app/helpers/random-loading-message.js
+++ app/helpers/random-loading-message.js
@@ -6,7 +6,6 @@ export function randomLoadingMessage() {
     "it's just a website",
     'this is BGS',
     'loading...',
-    'STUDIO PHONE inside UK: 0116 2581 545 outside UK: 44 116 2581 545',
     'greasy hotdogs...greasy fries...',
   ];
   return loadingMessages[Math.floor(Math.random() * loadingMessages.length)];
