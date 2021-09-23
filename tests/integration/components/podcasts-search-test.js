983ea8be8166382491f79eb03521cd3a57e96dd7
Tony Miller <mcfiredrill@gmail.com>
2021-09-22 19:24:46 -0700

    Revert "remove old component"
    This reverts commit af2ff72bc645d8398f9171a25a9e964c1604a8dc.

---
+++ tests/integration/components/podcasts-search-test.js
@@ -0,0 +1,27 @@
+import { module, skip } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Integration | Component | podcasts search', function (hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  // Called live endpoints, should be a mirage test or better mocked
+  skip('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+    this.set('searchParams', {});
+    this.set('tracks', []);
+    this.set('labels', []);
+
+    await render(hbs`<PodcastsSearch
+      @tracks={{tracks}}
+      @labels={{labels}}
+      @searchParams={{searchParams}}
+      />`);
+
+    assert.dom('*').hasText('');
+  });
+});
