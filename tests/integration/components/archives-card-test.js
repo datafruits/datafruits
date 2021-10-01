49714c54699fdaca938731333b9fe7e3ec3f26c5
Tony Miller <mcfiredrill@gmail.com>
2021-09-30 19:15:01 -0700

    fix lint

--- tests/integration/components/archives-card-test.js
+++ tests/integration/components/archives-card-test.js
@@ -3,17 +3,19 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | archives-card', function(hooks) {
+module('Integration | Component | archives-card', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     this.track = { title: 'shrimpshake 09202021' };
     this.image = 'cat.png';
-    this.scheduledShow = {id: 2};
-    await render(hbs`<ArchivesCard @track={{this.track}} @image={{this.image}} @scheduledShow={{this.scheduledShow}} />`);
+    this.scheduledShow = { id: 2 };
+    await render(
+      hbs`<ArchivesCard @track={{this.track}} @image={{this.image}} @scheduledShow={{this.scheduledShow}} />`,
+    );
 
     assert.dom(this.element).hasText('shrimpshake 09202021');
   });
