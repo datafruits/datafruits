3083bccb2da6649dfaffe69c78f11a87e95f3395
Tony Miller <mcfiredrill@gmail.com>
2021-10-07 15:16:07 -0700

    use loadRecords for podcast
    Really hacky and weird ugh

--- app/routes/home/index.js
+++ app/routes/home/index.js
@@ -15,10 +15,14 @@ export default class IndexRoute extends Route {
       start: dayjs().format('YYYY-MM-DD'),
       end: dayjs().endOf('month').add(1, 'month').format('YYYY-MM-DD'),
     };
-    const podcast = await this.store.queryRecord('podcast', {
-      name: 'datafruits',
-      page: 1,
-    });
+    const podcast = await this.store
+      .loadRecords('podcast', {
+        name: 'datafruits',
+        page: 1,
+      })
+      .then((podcasts) => {
+        return podcasts.get('firstObject');
+      });
     return hash({
       upcomingShows: this.store.loadRecords('scheduled-show', query).then((shows) => {
         return shows.slice(0, 6);
