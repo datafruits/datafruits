41ea51004fc05e63a7e874311cc1e8409843b08b
Tony Miller <mcfiredrill@gmail.com>
2021-11-02 20:05:26 -0700

    fix fruit-tip/fruit test

--- tests/integration/components/fruit-tip/fruit-test.js
+++ tests/integration/components/fruit-tip/fruit-test.js
@@ -3,24 +3,17 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | fruit-tip/fruit', function(hooks) {
+module('Integration | Component | fruit-tip/fruit', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.fruitTip = function () {};
+    this.fruit = {};
 
-    await render(hbs`<FruitTip::Fruit />`);
+    await render(hbs`<FruitTip::Fruit @fruitTip={{this.fruitTip}} @fruit={{this.fruit}} />`);
 
     assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <FruitTip::Fruit>
-        template block text
-      </FruitTip::Fruit>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
   });
 });
