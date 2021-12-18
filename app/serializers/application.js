3452623c36d3b1fcdcfb506f8877d565b8a7f9d0
Tony Miller <mcfiredrill@gmail.com>
2021-12-17 23:21:37 -0800

    delete comment

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -6,7 +6,6 @@ export default class Application extends ActiveModelSerializer {
   host = 'https://datafruits.streampusher.com';
 
   normalizeResponse(store, primaryModelClass, payload) {
-    //App.storeMeta[primaryModelClass.modelName] = payload.meta; //ember data only allows meta data on 'query', this adds support for all other methods
     return super.normalizeResponse(...arguments);
   }
 }
