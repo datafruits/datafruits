d03effa9b1acd4d917160c1f7107c27034ae516a
Tony Miller <mcfiredrill@gmail.com>
2022-01-11 10:58:25 -0800

    fix emoji-url-test

--- tests/integration/helpers/emoji-url-test.js
+++ tests/integration/helpers/emoji-url-test.js
@@ -3,15 +3,27 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Helper | emoji-url', function(hooks) {
+module('Integration | Helper | emoji-url', function (hooks) {
   setupRenderingTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test('it renders', async function (assert) {
+
+    this.set('inputValue', {
+      unicode: '1f33c'
+    });
 
     await render(hbs`{{emoji-url this.inputValue}}`);
 
-    assert.dom(this.element).hasText('1234');
+    assert.dom(this.element).hasText('https://cdn.jsdelivr.net/emojione/assets/4.0/png/32/1f33c.png');
+
+    this.set('inputValue', {
+      unicode: 'onion_salad_dressing',
+      animated: true,
+      custom: true
+    });
+
+    await render(hbs`{{emoji-url this.inputValue}}`);
+
+    assert.dom(this.element).hasText('/assets/images/emojis/onion_salad_dressing.gif');
   });
 });
