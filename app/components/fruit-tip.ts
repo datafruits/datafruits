a5c185d399ff86bbe2a215b1129010802f80d041
Tony Miller <mcfiredrill@gmail.com>
2022-01-31 21:40:42 -0800

    add types for fruit-tip component and fruit

--- app/components/fruit-tip.ts
+++ app/components/fruit-tip.ts
@@ -3,10 +3,10 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import fruitTypes from '../fruit-types';
+import ChatService from 'datafruits13/services/chat';
 
 export default class FruitTipComponent extends Component {
-  @service
-  chat;
+  @service declare chat: ChatService;
 
   @tracked fruitTypes = fruitTypes;
 
@@ -15,8 +15,7 @@ export default class FruitTipComponent extends Component {
   }
 
   @action
-  fruitTip(fruitType, event) {
-    console.log(this.chat.fruitCounts);
+  fruitTip(fruitType: string, event: PointerEvent) {
     event.preventDefault();
     if (this.chat.token) {
       this.chat.push('new:fruit_tip', {
