679ad9f0b39bc2e57fd326a4558eb2c6870481a6
Tony Miller <mcfiredrill@gmail.com>
2022-02-15 14:08:47 -0800

    add config/icons.js to use only needed fontawesome icons

---
+++ config/icons.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+  return {
+    'free-solid-svg-icons': ['bars', 'envelope', 'volume-off', 'volume-up', 'download', 'chevron-right', 'times'],
+    'free-brands-svg-icons': ['twitter', 'instagram', 'github', 'twitter', 'soundcloud', 'mixcloud'],
+  };
+};
