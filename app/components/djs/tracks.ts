c95c11a8b1737c7e298d63db25924afac742638c
Tony Miller <mcfiredrill@gmail.com>
2022-03-03 19:29:53 -0800

    paginate tracks on dj page

---
+++ app/components/djs/tracks.ts
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import fetch from 'fetch';
+import ENV from 'datafruits13/config/environment';
+import Store from '@ember-data/store';
+import RouterService from '@ember/routing/router-service';
+
+interface DjsTracksArgs {
+  page: string;
+  dj: void;
+}
+
+export default class DjsTracks extends Component<DjsTracksArgs> {
+  @service declare store: Store;
+  @service declare router: RouterService;
+
+  get djTracksUrl(): string {
+    // @ts-expect-error
+    return `${ENV.API_HOST}/api/djs/${this.args.dj.id}/tracks?page=${this.args.page}`;
+  }
+
+  @action
+  async fetchTracks() {
+    let response = await fetch(this.djTracksUrl);
+    return response.json();
+  }
+}
---
+++ types/datafruits13/config/environment.d.ts
@@ -0,0 +1,12 @@
+export default config;
+
+declare const config: {
+  environment: string;
+  modulePrefix: string;
+  podModulePrefix: string;
+  locationType: 'history' | 'hash' | 'none' | 'auto';
+  rootURL: string;
+  APP: Record<string, unknown>;
+  // our env vars
+  API_HOST: string;
+};
