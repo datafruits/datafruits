345a0d9d3f01a4f39fc9383fddfc271a5b3188f5
Tony Miller <mcfiredrill@gmail.com>
2022-03-28 17:22:03 -0700

    fix djs/tracks test

--- tests/integration/components/djs/tracks-test.js
+++ tests/integration/components/djs/tracks-test.js
@@ -9,18 +9,15 @@ module('Integration | Component | djs/tracks', function (hooks) {
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    //
+    this.page = '1';
 
-    await render(hbs`{{djs/tracks}}`);
+    this.dj = {
+      id: 1,
+    };
+
+    await render(hbs`<Djs::Tracks @dj={{this.dj}} @page={{this.page}} `);
 
     assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#djs/tracks}}
-        template block text
-      {{/djs/tracks}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
