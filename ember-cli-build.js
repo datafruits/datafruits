f6745503fcbc7218dddf6b8f5f0f49ebe48824a7
Tony Miller <mcfiredrill@gmail.com>
2022-06-16 14:36:22 +0900

    fix destroy favorite

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -97,6 +97,7 @@ module.exports = function (defaults) {
 
   app.import('/vendor/hinted-Debussy.woff2');
   app.import('/vendor/hinted-Debussy.woff');
+  app.import('/vendor/TopazPlus_a1200.woff2');
   app.import('node_modules/video.js/dist/video-js.min.css');
 
   return app.toTree();
