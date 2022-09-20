cc2eb101e1d6a03c1a8326c8e679ebc61f5ce473
Tony Miller <mcfiredrill@gmail.com>
2022-09-20 10:57:30 -0700

    remove ember-data-paperclip (#937)

--- app/controllers/home/user/settings.js
+++ app/controllers/home/user/settings.js
@@ -35,8 +35,22 @@ export default class HomeUserSettingsController extends Controller {
 
   @action
   updateFile(e) {
-    this.currentUser.user.avatar.update(e.target.files[0]);
+    // this.currentUser.user.avatar.update(e.target.files[0]);
+    // this.currentUser.user.avatarFilename = e.target.files[0].name;
+
+    this.file = e.target.files[0];
     this.currentUser.user.avatarFilename = e.target.files[0].name;
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+      this.currentUser.user.avatar = e.target.result;
+    };
+    reader.onerror = (e) => {
+      console.log('error reading file');
+      console.log(e);
+    };
+
+    reader.readAsDataURL(this.file);
   }
 
   @action
