f485f6e4c4025a1542276b170d1c32da2b0722db
Tony Miller <mcfiredrill@gmail.com>
2022-10-14 20:00:18 -0700

    make login modal draggable

--- app/components/login-modal.js
+++ app/components/login-modal.js
@@ -1,12 +1,44 @@
 import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
 import { action } from '@ember/object';
 
 export default class LoginModalComponent extends Component {
+  @tracked dragging = false;
+
   @action
   submit(event) {
     event.preventDefault();
     if (this.args.login(this.username, this.pass)) {
       this.args.toggleModal();
     }
+  }
+
+  @action
+  didInsert() {
+    console.log('adding event listeners');
+    document.addEventListener('mousedown', (event) => {
+      console.log('mousedown');
+      let modalTop = document.getElementById('login-modal-top');
+      if(event.target === modalTop) {
+        console.log('set dragging true', event.target);
+        this.dragging = true;
+        let loginModal = document.getElementById('login-modal');
+        loginModal.classList.add('dragging');
+      }
+    });
+    document.addEventListener('mouseup', (event) => {
+      console.log('mouseUp');
+      this.dragging = false;
+      let loginModal = document.getElementById('login-modal');
+      loginModal.classList.remove('dragging');
+    });
+    document.addEventListener('mousemove', (event) => {
+      if (this.dragging) {
+        console.log('setting x and y');
+        let loginModal = document.getElementById('login-modal');
+        loginModal.style.top = `${event.clientY}px`;
+        loginModal.style.left = `${event.clientX}px`;
+      }
+    });
   }
 }
---
+++ app/components/ui/modal.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+interface UiModalArgs {}
+
+export default class UiModal extends Component<UiModalArgs> {}
