eb4b7d4d7cb5b99a905c6748613c55d6649bed2b
Tony Miller <mcfiredrill@gmail.com>
2022-11-12 15:24:31 -0800

    fix fruit types

--- app/fruit-types.ts
+++ app/fruit-types.ts
@@ -7,6 +7,7 @@ const fruitTypes: Array<Fruit> = [
   new Fruit('banana', '/assets/images/emojis/banaynay.gif'),
   new Fruit('watermelon', '/assets/images/emojis/watermel.gif'),
   new Fruit('cabbage', '/assets/images/emojis/cabbage.gif'),
+  new Fruit('metalPineapple', '/assets/images/emojis/metal_pineapple.png'),
 ];
 
 export default fruitTypes;
