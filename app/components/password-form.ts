be8c284cccece8b9379ea668328399c8ded8f2c3
Tony Miller <mcfiredrill@gmail.com>
2022-12-07 15:44:44 -0800

    add token query param

--- app/components/password-form.ts
+++ app/components/password-form.ts
@@ -1,9 +1,22 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-interface PasswordFormArgs {}
+interface PasswordFormArgs {
+  token: string
+}
 
 export default class PasswordForm extends Component<PasswordFormArgs> {
+  @tracked password: string = "";
+  @tracked passwordConfirmation: string = "";
+
   get cantSubmit(): boolean {
     return false;
+  }
+
+  @action
+  submit(e: any) {
+    e.preventDefault();
+    console.log(this.args.token);
   }
 }
