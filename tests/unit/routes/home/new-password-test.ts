1a4c81137036cf8f4b912a69c8d2ce3de68fdefb
Tony Miller <mcfiredrill@gmail.com>
2022-12-08 14:56:32 -0800

    fix test

--- tests/unit/routes/home/new-password-test.ts
+++ tests/unit/routes/home/new-password-test.ts
@@ -5,7 +5,7 @@ module('Unit | Route | home/new-password', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:home/new-password');
+    const route = this.owner.lookup('route:home/new-password');
     assert.ok(route);
   });
 });
