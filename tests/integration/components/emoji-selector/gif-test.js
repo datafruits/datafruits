367c290cf665c4bae8d303d67d9a9f056d948ea9
Tony Miller <mcfiredrill@gmail.com>
2022-12-18 12:24:11 -0800

    fix test

--- tests/integration/components/emoji-selector/gif-test.js
+++ tests/integration/components/emoji-selector/gif-test.js
@@ -12,6 +12,6 @@ module('Integration | Component | emoji-selector/gif', function (hooks) {
 
     await render(hbs`<EmojiSelector::Gif />`);
 
-    assert.dom(this.element).hasText('search by tag');
+    assert.dom(this.element).hasText('search by tag Gifs stickers');
   });
 });
