7e025810f0b1d3f2f7aa9f7ceedc8d48576ecee3
Tony Miller <mcfiredrill@gmail.com>
2022-12-18 12:38:30 -0800

    set type to sticker always

--- app/serializers/sticker.js
+++ app/serializers/sticker.js
@@ -5,6 +5,7 @@ import JSONAPISerializer from '@ember-data/serializer/json-api';
 export default class Sticker extends JSONAPISerializer {
   normalizeQueryResponse(store, primaryModelClass, payload /*, id, requestType*/) {
     payload.data.map((gif) => {
+      gif.type = 'sticker';
       gif.attributes = {
         'preview-url': gif.images.fixed_width.url,
         url: gif.images.original.url,
