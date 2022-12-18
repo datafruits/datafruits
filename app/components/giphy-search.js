1a9a810d1de9a4ed03eb08c1f1613785fd210d12
Tony Miller <mcfiredrill@gmail.com>
2022-12-17 17:45:04 -0800

    add option to search giphy's sticker API
    Used the radio button, was the easiest way for now.

--- app/components/giphy-search.js
+++ app/components/giphy-search.js
@@ -1,9 +1,22 @@
 import { action } from "@ember/object";
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 
 export default class GiphySearch extends Component {
+  @tracked gifType = "gifs";
+
   @action
   focus(element) {
     element.children.namedItem("giphy-search-term").focus();
+  }
+
+  @action
+  useGifs() {
+    this.gifType = "gifs";
+  }
+
+  @action
+  useStickers() {
+    this.gifType = "stickers";
   }
 }
