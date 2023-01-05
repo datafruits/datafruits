225fbbede40a5d496b908e19b2ef939c47c01468
Tony Miller <mcfiredrill@gmail.com>
2023-01-05 11:15:11 -0800

    show preview immediately for wiki edit

--- app/components/wiki/form.ts
+++ app/components/wiki/form.ts
@@ -8,9 +8,10 @@ import { tracked } from '@glimmer/tracking';
 import emojione from 'emojione';
 import { SafeString } from 'handlebars';
 import { htmlSafe } from '@ember/template';
+import type WikiPage from 'datafruits13/models/wiki-page';
 
 interface WikiFormArgs {
-  changeset: any;
+  model: WikiPage;
 }
 
 export default class WikiForm extends Component<WikiFormArgs> {
@@ -21,6 +22,10 @@ export default class WikiForm extends Component<WikiFormArgs> {
 
   @tracked previewBody: SafeString = new SafeString('');
   @tracked previewTitle: SafeString = new SafeString('');
+
+  @action didInsert() {
+    this.previewBody = htmlSafe(emojione.shortnameToImage(this.args.model.body));
+  }
 
   @action
   setPreviewBody(value: string) {
