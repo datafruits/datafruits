456eff8e00b51e2cb9fb42f7dc1e84d77d1e5768
Tony Miller <mcfiredrill@gmail.com>
2023-01-17 17:13:58 -0800

    format date

--- app/components/post.ts
+++ app/components/post.ts
@@ -3,6 +3,14 @@ import { htmlSafe } from '@ember/template';
 import { SafeString } from 'handlebars';
 import emojione from 'emojione';
 import type PostModel from 'datafruits13/models/post';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(localizedFormat);
 
 interface PostArgs {
   post: PostModel;
@@ -11,5 +19,10 @@ interface PostArgs {
 export default class Post extends Component<PostArgs> {
   get body(): SafeString {
     return htmlSafe(emojione.shortnameToImage(this.args.post.body));
+  }
+
+  get formattedDate(): string {
+    const timeZone = dayjs.tz.guess();
+    return dayjs(this.args.post.createdAt).tz(timeZone).format('LLL');
   }
 }
