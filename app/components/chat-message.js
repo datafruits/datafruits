4f5ea32444201c9fe323415267fb1c497d5452e9
Dakota Clark <dakotamusic@gmail.com>
2023-03-05 02:52:34 -0500

    check if the message is from discord and reformat

--- app/components/chat-message.js
+++ app/components/chat-message.js
@@ -5,16 +5,7 @@ import { action } from '@ember/object';
 export default class ChatMessage extends Component {
   @tracked gifsEnabled = true;
   @tracked imgRegex = /https?:\/\/(?:[a-z0-9-]+\.)+[a-z]{2,6}(?:\/[^/#?]+)+\.(?:jpg|gif|png|webp)(\?.*$)*/;
-
-  get canShowAvatar() {
-    if (!this.args.message.role) return false;
-    return (
-      this.args.message.role.includes('dj') ||
-      this.args.message.role.includes('admin') ||
-      this.args.message.role.includes('listener') ||
-      this.args.message.role.includes('bot')
-    );
-  }
+  @tracked discordRegex = /^New\ msg\ in\ discord\ from\ (.+):\ (.+)$/;
 
   get hasImage() {
     return this.imgRegex.test(this.args.message.body);
@@ -22,6 +13,18 @@ export default class ChatMessage extends Component {
 
   get imgUrl() {
     return this.args.message.body.match(this.imgRegex)[0];
+  }
+
+  get fromDiscord() {
+    return this.discordRegex.test(this.args.message.body);
+  }
+  
+  get discordUser() {
+    return this.args.message.body.match(this.discordRegex)[1];
+  }
+
+  get discordMsg() {
+    return this.args.message.body.match(this.discordRegex)[2];
   }
 
   @action
