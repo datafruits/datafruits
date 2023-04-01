2efc3754481ab08181637b9cd93b7cd5b5698e32
Tony Miller <mcfiredrill@gmail.com>
2023-03-31 21:53:28 -0700

    move modal logic to ui/modal

--- app/components/login-modal.js
+++ app/components/login-modal.js
@@ -1,11 +1,8 @@
 import Component from '@glimmer/component';
 import { htmlSafe } from '@ember/template';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class LoginModalComponent extends Component {
-  @tracked dragging = false;
-
   @action
   submit(event) {
     event.preventDefault();
@@ -14,40 +11,4 @@ export default class LoginModalComponent extends Component {
     }
   }
 
-  clickedOffsetX = 0;
-  clickedOffsetY = 0;
-
-  @action
-  didInsert() {
-    console.log('adding event listeners');
-    document.addEventListener('mousedown', (event) => {
-      console.log('mousedown', event);
-      this.clickedOffsetX = event.offsetX;
-      this.clickedOffsetY = event.offsetY;
-      let modalTop = document.getElementById('login-modal-top');
-      if(event.target === modalTop) {
-        console.log('set dragging true', event.target);
-        this.dragging = true;
-        let loginModal = document.getElementById('login-modal');
-        if(loginModal) {
-          loginModal.classList.add('dragging');
-        }
-      }
-    });
-    document.addEventListener('mouseup', (event) => {
-      console.log('mouseUp');
-      this.dragging = false;
-      let loginModal = document.getElementById('login-modal');
-      loginModal.classList.remove('dragging');
-    });
-    document.addEventListener('mousemove', (event) => {
-      if (this.dragging) {
-        console.log('setting x and y', event);
-        let loginModal = document.getElementById('login-modal');
-        loginModal.style.top = `${event.clientY - this.clickedOffsetY}px`;
-        loginModal.style.left = `${event.clientX - this.clickedOffsetX}px`;
-        loginModal.style.transform = 'none';
-      }
-    });
-  }
 }
--- app/components/ui/modal.ts
+++ app/components/ui/modal.ts
@@ -1,5 +1,51 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 interface UiModalArgs {}
 
-export default class UiModal extends Component<UiModalArgs> {}
+export default class UiModal extends Component<UiModalArgs> {
+  @tracked dragging = false;
+
+  clickedOffsetX = 0;
+  clickedOffsetY = 0;
+
+  @action
+  didInsert() {
+    console.log('adding event listeners');
+    document.addEventListener('mousedown', (event) => {
+      console.log('mousedown', event);
+      this.clickedOffsetX = event.offsetX;
+      this.clickedOffsetY = event.offsetY;
+      let modalTop = document.getElementById('login-modal-top'); // HOW TO GET UNIQUE ID?!!!
+      if(event.target === modalTop) {
+        console.log('set dragging true', event.target);
+        this.dragging = true;
+        let modal = document.getElementById('login-modal');
+        if(modal) {
+          modal.classList.add('dragging');
+        }
+      }
+    });
+    document.addEventListener('mouseup', (event) => {
+      console.log('mouseUp');
+      this.dragging = false;
+      let modal = document.getElementById('login-modal') as HTMLElement;
+      modal.classList.remove('dragging');
+    });
+    document.addEventListener('mousemove', (event) => {
+      if (this.dragging) {
+        console.log('setting x and y', event);
+        let modal = document.getElementById('login-modal');
+        modal.style.top = `${event.clientY - this.clickedOffsetY}px`;
+        modal.style.left = `${event.clientX - this.clickedOffsetX}px`;
+        modal.style.transform = 'none';
+      }
+    });
+  }
+
+  @action
+  removeListeners() {
+    // TODO remove event listeners
+  }
+}
