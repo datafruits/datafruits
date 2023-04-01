573cf7f8217a469f9c7b9f82bda330f8bad153f9
Tony Miller <mcfiredrill@gmail.com>
2023-03-31 22:26:50 -0700

    beeeeeeeeeeeeeeeeeeep

--- app/components/ui/modal.ts
+++ app/components/ui/modal.ts
@@ -2,41 +2,41 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
-interface UiModalArgs {}
+//interface UiModalArgs {}
 
-export default class UiModal extends Component<UiModalArgs> {
+export default class UiModal extends Component {
   @tracked dragging = false;
 
   clickedOffsetX = 0;
   clickedOffsetY = 0;
 
   @action
-  didInsert() {
+  didInsert(element: any) {
     console.log('adding event listeners');
     document.addEventListener('mousedown', (event) => {
       console.log('mousedown', event);
       this.clickedOffsetX = event.offsetX;
       this.clickedOffsetY = event.offsetY;
-      let modalTop = document.getElementById('login-modal-top'); // HOW TO GET UNIQUE ID?!!!
+      const modalTop = document.getElementById('login-modal-top') as HTMLElement; // HOW TO GET UNIQUE ID?!!!
       if(event.target === modalTop) {
         console.log('set dragging true', event.target);
         this.dragging = true;
-        let modal = document.getElementById('login-modal');
+        const modal = document.getElementById('login-modal') as HTMLElement;
         if(modal) {
           modal.classList.add('dragging');
         }
       }
     });
-    document.addEventListener('mouseup', (event) => {
+    document.addEventListener('mouseup', () => {
       console.log('mouseUp');
       this.dragging = false;
-      let modal = document.getElementById('login-modal') as HTMLElement;
+      const modal = document.getElementById('login-modal') as HTMLElement;
       modal.classList.remove('dragging');
     });
-    document.addEventListener('mousemove', (event) => {
+    document.addEventListener('mousemove', (event: any) => {
       if (this.dragging) {
         console.log('setting x and y', event);
-        let modal = document.getElementById('login-modal');
+        const modal = document.getElementById('login-modal') as HTMLElement;
         modal.style.top = `${event.clientY - this.clickedOffsetY}px`;
         modal.style.left = `${event.clientX - this.clickedOffsetX}px`;
         modal.style.transform = 'none';
