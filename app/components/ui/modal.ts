b0c25b6d14480e3b396b52972a836aa18991c97b
Tony Miller <mcfiredrill@gmail.com>
2023-04-10 20:52:21 -0700

    render all modals inside a div at the bottom using in-element helper
    This makes sure the dragging offsets work properly

--- app/components/ui/modal.ts
+++ app/components/ui/modal.ts
@@ -15,31 +15,21 @@ export default class UiModal extends Component<UiModalArgs> {
   @action
   didInsert(element: HTMLElement) {
     element.addEventListener('mousedown', (event) => {
-      console.log('setting offset', event);
       this.clickedOffsetX = event.offsetX;
       this.clickedOffsetY = event.offsetY;
       const modalTop = element.querySelector('.modal-top') as HTMLElement;
-      console.log('event.target', event.target);
-      console.log(modalTop);
       if(event.target === modalTop) {
-        console.log('set dragging true', event.target);
         this.dragging = true;
         element.classList.add('dragging');
       }
     });
     document.addEventListener('mouseup', () => {
-      console.log('mouseUp');
       this.dragging = false;
       element.classList.remove('dragging');
     });
     document.addEventListener('mousemove', (event: any) => {
       if (this.dragging) {
-        console.log('setting x and y', event);
-        console.log('offsetY', this.clickedOffsetY);
-        console.log('event.pageY - offsetY', event.pageY - this.clickedOffsetY);
         const modal = element as HTMLElement;
-        // modal.style.transform = 'none';
-        //modal.style.top = `${event.clientY - this.clickedOffsetY}px`;
         modal.style.top = `${event.clientY - (this.clickedOffsetY)}px`;
         modal.style.left = `${event.clientX - this.clickedOffsetX}px`;
       }
@@ -51,5 +41,9 @@ export default class UiModal extends Component<UiModalArgs> {
     //window.removeEventListener('mousemove');
     //window.removeEventListener('mouseup');
     //window.removeEventListener('mousedown');
+  }
+
+  get destinationElement() {
+    return document.getElementById("modals-container");
   }
 }
