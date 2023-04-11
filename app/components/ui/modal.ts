077e3eb95cec91b2173ee0861a7bc1a7b0f44963
Tony Miller <mcfiredrill@gmail.com>
2023-04-11 08:41:25 -0700

    clean up accessing _element

--- app/components/ui/modal.ts
+++ app/components/ui/modal.ts
@@ -12,31 +12,34 @@ export default class UiModal extends Component<UiModalArgs> {
   clickedOffsetX = 0;
   clickedOffsetY = 0;
 
+  _element: HTMLElement | null = null;
+
   _mousedownListener = (event: MouseEvent) => {
     this.clickedOffsetX = event.offsetX;
     this.clickedOffsetY = event.offsetY;
-    const modalTop = this.element.querySelector('.modal-top') as HTMLElement;
+    const modalTop = (this._element as HTMLElement).querySelector('.modal-top') as HTMLElement;
     if(event.target === modalTop) {
       this.dragging = true;
-      this.element.classList.add('dragging');
+      (this._element as HTMLElement).classList.add('dragging');
     }
   };
 
   _mousemoveListener = (event: MouseEvent) => {
     if (this.dragging) {
-      const modal = this.element as HTMLElement;
+      const modal = this._element as HTMLElement;
       modal.style.top = `${event.clientY - (this.clickedOffsetY)}px`;
       modal.style.left = `${event.clientX - this.clickedOffsetX}px`;
     }
   };
 
-  _mouseupListener = (_event: MouseEvent) => {
+  _mouseupListener = () => {
       this.dragging = false;
-      this.element.classList.remove('dragging');
+      (this._element as HTMLElement).classList.remove('dragging');
   };
 
   @action
   didInsert(element: HTMLElement) {
+    this._element = element;
     element.addEventListener('mousedown', this._mousedownListener);
     document.addEventListener('mouseup', this._mouseupListener);
     document.addEventListener('mousemove', this._mousemoveListener);
