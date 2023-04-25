734e0d1de48b9df8d6ed40d22c58ad0a87c19942
Tony Miller <mcfiredrill@gmail.com>
2023-04-24 20:27:32 -0700

    lint fix

--- tests/unit/routes/home/user/notifications-test.ts
+++ tests/unit/routes/home/user/notifications-test.ts
@@ -5,7 +5,7 @@ module('Unit | Route | home/user/notifications', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:home/user/notifications');
+    const route = this.owner.lookup('route:home/user/notifications');
     assert.ok(route);
   });
 });
