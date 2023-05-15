4f19ed30e76e5164f31794e9e160cecd06bf3e93
Tony Miller <mcfiredrill@gmail.com>
2023-05-14 20:46:03 -0700

    level req for pineapple

---
+++ app/components/fruit-tip/fruit.ts
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import type Fruit from '../../fruit';
+import CurrentUserService from 'datafruits13/services/current-user';
+import ChatService from 'datafruits13/services/chat';
+
+interface FruitTipFruitComponentArgs {
+  fruit: Fruit;
+  cost: number;
+  fruitTip: Function;
+}
+
+export default class FruitTipFruitComponent extends Component<FruitTipFruitComponentArgs> {
+  @service declare chat: ChatService;
+  @service declare currentUser: CurrentUserService;
+  @service declare session: any;
+
+  get count() {
+    console.log(`in count: ${this.args.fruit.name}`);
+    return this.chat.getFruitCount(this.args.fruit.name);
+  }
+
+  get disabled() {
+    return (this.currentUser.user.level < this.args.fruit.levelReq) ||
+      (this.currentUser.user.fruitTicketBalance < this.args.fruit.cost) ||
+      (!this.session.isAuthenticated);
+  }
+}
