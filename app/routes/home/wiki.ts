33b3c9f49ed4af817de825f1308b54b076e3708a
Tony Miller <mcfiredrill@gmail.com>
2023-06-02 11:52:08 -0700

    fix wiki route

--- app/routes/home/wiki.ts
+++ app/routes/home/wiki.ts
@@ -1,10 +1,11 @@
 import Route from '@ember/routing/route';
-import { service } from '@ember/service';
-import type StoreService from '@ember-data/store';
+// FIXME this causes an error when rendering the link to route
+// maybe it's fixed in a newer ember version??
+// import { service } from '@ember/service';
+// import type StoreService from '@ember-data/store';
 
 export default class HomeWiki extends Route {
-  @service declare store: StoreService;
-  // normal class body definition here
+  // @service declare store: StoreService;
   model() {
     return this.store.findAll('wikiPage');
   }
