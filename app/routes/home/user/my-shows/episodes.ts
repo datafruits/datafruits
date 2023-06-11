9ec4b2808f16fba839599dab94c2471f2dbceffd
Tony Miller <mcfiredrill@gmail.com>
2023-06-11 14:21:51 -0700

    load episodes async on my-shows/

--- app/routes/home/user/my-shows/episodes.ts
+++ app/routes/home/user/my-shows/episodes.ts
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
+// https://docs.ember-cli-typescript.com/ember/routes
+type Resolved<P> = P extends Promise<infer T> ? T : P;
+export type HomeUserMyShowsEpisodesRouteModel = Resolved<ReturnType<HomeUserMyShowsEpisodes['model']>>;
 
 export default class HomeUserMyShowsEpisodes extends Route {
   model(params: any) {
