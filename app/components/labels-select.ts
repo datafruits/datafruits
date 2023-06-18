93aa5080afb631fb61de12481a85ef06aad6ce61
Tony Miller <mcfiredrill@gmail.com>
2023-06-18 10:58:11 -0700

    debounce labels search

--- app/components/labels-select.ts
+++ app/components/labels-select.ts
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { BufferedChangeset } from 'ember-changeset/types';
+import { debounce } from '@ember/runloop';
 import type Label from 'datafruits13/models/label';
 
 interface LabelsSelectArgs {
@@ -49,8 +50,21 @@ export default class LabelsSelect extends Component<LabelsSelectArgs> {
 
   @action
   searchLabels(term: string) {
+    return new Promise((resolve, reject) => {
+      debounce(this, this._performLabelsSearch, term, resolve, reject, 600);
+    });
+  }
+
+  _performLabelsSearch(term: string, resolve: any, reject: any) {
     this.store.query('label', { term: term }).then((labels: Label[]) => {
       this.labels = labels;
-    });
+      return resolve(labels);
+    }, reject);
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    LabelsSelect: typeof LabelsSelect;
   }
 }
