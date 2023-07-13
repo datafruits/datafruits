bde721260c97db391ab67b04562fe25e0ec71eb1
Tony Miller <mcfiredrill@gmail.com>
2023-07-13 16:46:11 -0700

    create support page
    also replaced ampled copy with patreon since ampled is shutting down

--- app/router.js
+++ app/router.js
@@ -42,6 +42,7 @@ Router.map(function () {
     });
     this.route('forum.new', { path: '/forum/new' });
     this.route('forum.show', { path: '/forum/:title' });
+    this.route('support');
   });
   this.route('container', function () {
     this.route('show', { path: '/shows/:id' });
