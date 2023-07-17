4c2024578b190d71d0055c838b166a1a7c5b6f44
Tony Miller <mcfiredrill@gmail.com>
2023-07-16 17:52:16 -0700

    start acceptance test

---
+++ tests/acceptance/schedule-show-test.js
@@ -0,0 +1,25 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Acceptance | schedule show', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('schedule new show series', async function (assert) {
+    window.alert = () => {};
+
+    await visit('/chat');
+    await click('[data-test-login-button]');
+
+    await fillIn('[data-test-username]', 'djnameko');
+    await fillIn('[data-test-password]', 'mypassword1234');
+
+    await click('[data-test-login-submit]');
+
+    assert.equal(currentURL(), '/chat');
+
+    await visit('/user/my-shows/');
+  });
+});
