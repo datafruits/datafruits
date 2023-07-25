61738f5dfe617b187a3fb6e0bb81424624cd11de
Tony Miller <mcfiredrill@gmail.com>
2023-07-25 13:30:21 -0700

    fix my-shows/episode-card

--- app/components/my-shows/episode-card.ts
+++ app/components/my-shows/episode-card.ts
@@ -1,3 +1,4 @@
+import { htmlSafe } from '@ember/template';
 import Component from '@glimmer/component';
 import type ScheduledShow from 'datafruits13/models/scheduled-show';
 
@@ -5,7 +6,22 @@ interface MyShowsEpisodeCardArgs {
   episode: ScheduledShow;
 }
 
-export default class MyShowsEpisodeCard extends Component<MyShowsEpisodeCardArgs> {}
+export default class MyShowsEpisodeCard extends Component<MyShowsEpisodeCardArgs> {
+  get backgroundStyle() {
+    let image;
+    const show = this.args.episode;
+    if (show.thumbImageUrl) {
+      image = show.thumbImageUrl;
+    // } else if (show.isGuest) {
+    //   image = '/assets/images/show_placeholder.jpg';
+    // } else if (show.host && show.host.imageUrl) {
+    //   image = show.host.imageUrl;
+    } else {
+      image = '/assets/images/show_placeholder.jpg';
+    }
+    return htmlSafe(`background-image: url('${image}');`);
+  }
+}
 
 
 declare module '@glint/environment-ember-loose/registry' {
