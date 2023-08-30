05f6f27d67e562b50c92d49fe33b840f7c81be99
Tony Miller <mcfiredrill@gmail.com>
2023-08-29 21:09:27 -0700

    fix chat-settings empty signature

--- app/components/chat/chat-settings.ts
+++ app/components/chat/chat-settings.ts
@@ -5,15 +5,11 @@ import { tracked } from '@glimmer/tracking';
 import { formatEmojiHtml } from 'datafruits13/helpers/format-emoji-html';
 import ChatService from 'datafruits13/services/chat';
 
-interface ChatChatSettingsSignature {
-  Args: {};
-}
-
-export default class ChatSettings extends Component<ChatChatSettingsSignature> {
+export default class ChatSettings extends Component {
   @service declare chat: ChatService;
 
   @tracked declare showingDialog: boolean;
-  
+
   get gearIcon() {
     return formatEmojiHtml(":gear:")
   }
@@ -27,7 +23,7 @@ export default class ChatSettings extends Component<ChatChatSettingsSignature> {
   closeDialog() {
     this.showingDialog = false;
   }
-  
+
   @action
   toggleGifsEnabled() {
     this.chat.gifsEnabled = !this.chat.gifsEnabled;
@@ -40,4 +36,4 @@ declare module '@glint/environment-ember-loose/registry' {
     ChatSettings: typeof ChatSettings;
   }
 }
-  
+
