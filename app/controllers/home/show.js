a70a4787f3d6537164a2798fd07598f8fe022710
Tony Miller <mcfiredrill@gmail.com>
2023-09-27 23:02:05 -0700

    show edit button on show if logged in and its your show

--- app/controllers/home/show.js
+++ app/controllers/home/show.js
@@ -1,9 +1,13 @@
 import classic from 'ember-classic-decorator';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 @classic
 export default class ShowController extends Controller {
+  @service session;
+  @service currentUser;
+
   @action
   browseLabel(label) {
     this.transitionToRoute('home.podcasts', { queryParams: { tags: label.name } });
