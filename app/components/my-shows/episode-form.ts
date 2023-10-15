f3d45cadb6d8da6046dbbca05ee737da36e230d1
Tony Miller <mcfiredrill@gmail.com>
2023-10-14 20:03:55 -0700

    fix checkbox for prerecord archive
    use regular select tag for archive status

--- app/components/my-shows/episode-form.ts
+++ app/components/my-shows/episode-form.ts
@@ -12,10 +12,10 @@ export default class MyShowsEpisodeForm extends Component<MyShowsEpisodeFormArgs
 
   file: Blob | null = null;
 
-  statusOptions: string[] = [
-    "archive_published",
-    "archive_unpublished"
-  ];
+  statusOptions = {
+    "Published": "archive_published",
+    "Unpublished": "archive_unpublished"
+  };
 
   @action updateFile(e: any){
     this.file = e.target.files[0];
