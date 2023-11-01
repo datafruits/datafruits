b528a5b8306134a31a0b2e32062d30bf834b0aaa
Tony Miller <mcfiredrill@gmail.com>
2023-11-01 10:17:09 -0700

    fix podcast pagination

--- app/components/podcasts.ts
+++ app/components/podcasts.ts
@@ -1,19 +1,23 @@
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import type Store from '@ember-data/store';
 import type RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 //import { hash } from 'rsvp';
 import Component from '@glimmer/component';
 
-interface PodcastsSearchArgs {
+interface PodcastsArgs {
   labels: [any];
   query: any;
+  page: number;
 }
 
-export default class PodcastsSearch extends Component<PodcastsSearchArgs> {
+export default class PodcastsSearch extends Component<PodcastsArgs> {
   @service declare store: Store;
 
   @service declare router: RouterService;
+
+  @tracked initialData: any;
 
   get labelNames() {
     return this.args.labels.map(function (label) {
@@ -37,6 +41,17 @@ export default class PodcastsSearch extends Component<PodcastsSearchArgs> {
 
     return podcastsPromise;
   }
+
+  @action
+  fetchInitial() {
+    const query = this.args.query;
+    const podcastsPromise = this.store.query('podcast', query);
+
+    return podcastsPromise.then((podcasts) => {
+      this.initialData = podcasts;
+    });
+  }
+
 }
 
 declare module '@glint/environment-ember-loose/registry' {
