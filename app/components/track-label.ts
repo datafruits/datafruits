369a327c7fabf87f0ba94abf6da39975e9e89b97
Tony Miller <mcfiredrill@gmail.com>
2023-11-15 21:24:11 -0800

    link to archives page from labels

--- app/components/track-label.ts
+++ app/components/track-label.ts
@@ -1,5 +1,9 @@
 import Component from '@glimmer/component';
+import type Label from 'datafruits13/models/label';
 
-interface TrackLabelArgs {}
+interface TrackLabelArgs {
+  label: Label;
+}
 
-export default class TrackLabel extends Component<TrackLabelArgs> {}
+export default class TrackLabel extends Component<TrackLabelArgs> {
+}
