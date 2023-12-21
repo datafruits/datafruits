42d32cf4a408b1307d43516ad89f57a1bd4002ec
dudethatbe <dakotamusic@gmail.com>
2023-12-21 17:56:55 -0500

    weather control (#1245)
    * add weather control and pretty it up
    
    * refactor event handler
    
    * delete test
    
    * fix vscode ts errors
    
    * idk make it js
    
    * add curly braces for eslint lexical scoping

---
+++ app/services/weather.ts
@@ -0,0 +1,17 @@
+import Service from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+
+export default class Weather extends Service {
+  setWeather(weather: string) {
+    this.currentWeather = weather;
+  }
+
+  @tracked currentWeather: string = "snowy";
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your services.
+declare module "@ember/service" {
+  interface Registry {
+    weather: Weather;
+  }
+}
