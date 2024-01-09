45c0d9138b05ecf1acf8a265669386a8936d6783
Dakota Clark <dakotamusic@gmail.com>
2024-01-08 22:16:45 -0500

    render emojis in bio

--- app/controllers/home/dj.js
+++ app/controllers/home/dj.js
@@ -2,15 +2,24 @@ import classic from 'ember-classic-decorator';
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { htmlSafe } from "@ember/template";
+import { SafeString } from "handlebars";
+import emojione from "emojione";
 
 @classic
 export default class DjController extends Controller {
   @service router;
-  queryParams = ['page'];
+  queryParams = ["page"];
 
   @action
   browseLabel(label) {
-    this.transitionToRoute('home.podcasts', { queryParams: { tags: label.name } });
+    this.transitionToRoute("home.podcasts", {
+      queryParams: { tags: label.name },
+    });
+  }
+
+  get modelBio() {
+    return htmlSafe(emojione.shortnameToImage(this.model.bio));
   }
 
   get qualifiedHomepageUrl() {
