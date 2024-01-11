2d570000730a200836fa702606d1a7bb7be13718
Dakota Clark <dakotamusic@gmail.com>
2024-01-11 01:13:19 -0500

    import service and make it smaller

--- app/components/weather-selector.js
+++ app/components/weather-selector.js
@@ -1,7 +1,11 @@
+import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 
 export default class WeatherSelector extends Component {
+  @service
+  weather;
+
   weathers = [
     {
       text: "☁",
