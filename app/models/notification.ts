dbe14864bdd3433fbf9fcb4882a91439c14550b8
Tony Miller <mcfiredrill@gmail.com>
2024-01-15 18:14:29 -0800

    add url to notification

--- app/models/notification.ts
+++ app/models/notification.ts
@@ -2,7 +2,7 @@ import Model, { attr } from '@ember-data/model';
 
 export default class Notification extends Model {
   @attr('string') declare message: string;
+  @attr('string') declare url: string;
 
   @attr('string') declare createdAt: string;
 }
-
