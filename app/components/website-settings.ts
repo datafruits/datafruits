b760e9559a1acee7b297bf435d10b64ae20eb75f
Dakota Clark <dakotamusic@gmail.com>
2024-01-20 00:55:53 -0500

    theme and weather control dont work yet

--- app/components/website-settings.ts
+++ app/components/website-settings.ts
@@ -3,12 +3,31 @@ import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 
 import ChatService from "datafruits13/services/chat";
+import ThemeService from "datafruits13/services/theme";
+import Weather from "datafruits13/services/weather";
 
 export default class WebsiteSettings extends Component {
+  @service declare intl: any;
+  @service declare theme: ThemeService;
+  @service declare weather: Weather;
   @service declare chat: ChatService;
 
   @action
   toggleGifsEnabled() {
     this.chat.gifsEnabled = !this.chat.gifsEnabled;
+  }
+  @action
+  setLocale(locale: any) {
+    this.intl.locale = locale;
+  }
+
+  @action
+  setTheme(theme: any) {
+    this.theme.setTheme(theme);
+  }
+
+  @action
+  setWeather(event: any) {
+    this.weather.setWeather(event.target.value);
   }
 }
