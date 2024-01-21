c81920ab6ee2358a2a2b2143c44bc3e1220dcb03
Dakota Clark <dakotamusic@gmail.com>
2024-01-21 00:12:07 -0500

    change buddy list button to checkbox

--- app/components/chat/buddy-list-button.ts
+++ app/components/chat/buddy-list-button.ts
@@ -1,9 +1,8 @@
-import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { formatEmojiHtml } from 'datafruits13/helpers/format-emoji-html';
-import ChatService from 'datafruits13/services/chat';
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import ChatService from "datafruits13/services/chat";
 
 export default class BuddyListButton extends Component {
   @service declare chat: ChatService;
@@ -13,16 +12,10 @@ export default class BuddyListButton extends Component {
   @action toggleDialog() {
     this.showingDialog = !this.showingDialog;
   }
-
-  get buddyListIcon() {
-    return formatEmojiHtml(":busts_in_silhouette:");
-  }
 }
 
-
-declare module '@glint/environment-ember-loose/registry' {
+declare module "@glint/environment-ember-loose/registry" {
   export default interface Registry {
     BuddyListButton: typeof BuddyListButton;
   }
 }
-
