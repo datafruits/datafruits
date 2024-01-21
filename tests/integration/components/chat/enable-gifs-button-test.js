651fe713b1ea36169e582bfce0341fb22f14075f
Dakota Clark <dakotamusic@gmail.com>
2024-01-20 22:59:51 -0500

    ok maybe test fixed now

--- tests/integration/components/chat/enable-gifs-button-test.js
+++ tests/integration/components/chat/enable-gifs-button-test.js
@@ -14,7 +14,10 @@ module('Integration | Component | chat/enable-gifs-button', function (hooks) {
       hbs`<Chat::EnableGifsButton @toggleGifs={{this.toggleGifs}} @enabled={{this.gifsEnabled}} />`,
     );
 
-    assert.dom(this.element).exists;
+    assert.dom(this.element).exists("GIF enabler should exist");
+    assert
+      .dom(this.element)
+      .isChecked("GIF enabler should be checked by default");
   
   });
 });
