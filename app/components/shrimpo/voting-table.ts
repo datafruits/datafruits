4ca4b4a3e7bcf274dec87fe8d656dc6ceb7dd686
Tony Miller <mcfiredrill@gmail.com>
2024-02-15 12:07:26 -0800

    add voting slider thingy

--- app/components/shrimpo/voting-table.ts
+++ app/components/shrimpo/voting-table.ts
@@ -1,8 +1,22 @@
 import Component from '@glimmer/component';
 import type ShrimpoEntry from 'datafruits13/models/shrimpo-entry';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+//import { formatEmojiHtml } from 'datafruits13/helpers/format-emoji-html';
 
 interface ShrimpoVotingTableArgs {
   entry: ShrimpoEntry;
 }
 
-export default class ShrimpoVotingTable extends Component<ShrimpoVotingTableArgs> {}
+export default class ShrimpoVotingTable extends Component<ShrimpoVotingTableArgs> {
+  @tracked score: number = 1;
+
+  @action
+  setScore(event: any) {
+    this.score = event.target.value;
+  }
+
+  // get scoreEmoji() {
+  //   return formatEmojiHtml(this.args.entry.shrimpo.emoji);
+  // }
+}
