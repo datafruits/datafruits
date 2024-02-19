65a047b3c2260988135ad279eead7722b2fc2c19
Tony Miller <mcfiredrill@gmail.com>
2024-02-19 14:21:55 -0800

    initialize shrimpo-vote

--- app/components/shrimpo/voting-table.ts
+++ app/components/shrimpo/voting-table.ts
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import type ShrimpoEntry from 'datafruits13/models/shrimpo-entry';
+import type ShrimpoVote from 'datafruits13/models/shrimpo-vote';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { formatEmojiHtml } from 'datafruits13/helpers/format-emoji-html';
@@ -16,20 +17,39 @@ export default class ShrimpoVotingTable extends Component<ShrimpoVotingTableArgs
   @service
   declare store: any;
 
+  @service
+  declare currentUser: any;
+
   @tracked score: number = 1;
+
+  vote: ShrimpoVote;
+
+  constructor(owner: unknown, args: any) {
+    super(owner, args);
+    const existingVote = this.args.entry.shrimpoVotes.find((vote: ShrimpoVote) => {
+      return vote.user == this.currentUser.user;
+    });
+    if(existingVote) {
+      this.vote = existingVote;
+    } else {
+      this.vote = this.store.createRecord('shrimpo-vote', {
+        shrimpoEntry: this.args.entry,
+        score: this.score
+      });
+    }
+  }
 
   @action
   async saveVote() {
-    let vote = this.store.createRecord('shrimpo-vote', {
-      shrimpoEntry: this.args.entry,
-      score: this.score
-    });
-    console.log(vote.get('shrimpoEntry'));
+    console.log(this.vote.get('shrimpoEntry'));
     try {
-      await vote.save();
+      await this.vote.save();
+      console.log(this.vote);
       alert('saved shrimpo vote!');
     } catch (error) {
+      console.log(this.vote);
       console.log(error);
+      console.trace();
       alert('couldnt save vote!');
     }
   }
