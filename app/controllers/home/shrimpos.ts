5b5d5eb97a6511fdc85d1e87f1f09b6747faaa4f
Tony Miller <mcfiredrill@gmail.com>
2024-02-20 08:30:09 -0800

    only show saved shrimpos on index

--- app/controllers/home/shrimpos.ts
+++ app/controllers/home/shrimpos.ts
@@ -1,8 +1,17 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import type Shrimpo from 'datafruits13/models/shrimpo';
 
 export default class HomeShrimpos extends Controller {
+  declare model: [Shrimpo];
+
   @service declare session: any;
+
+  get savedShrimpos() {
+    return this.model.filter((shrimpo: any) => {
+      return !shrimpo.isNew;
+    });
+  }
 }
 
 // DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
