537857e229a71b2ab5d74e760958d9f28ce3cf2f
Tony Miller <mcfiredrill@gmail.com>
2024-03-23 00:10:35 -0700

    can't vote on own shrimpo entry (#1328)

--- app/controllers/home/shrimpos/entries/show.ts
+++ app/controllers/home/shrimpos/entries/show.ts
@@ -1,9 +1,11 @@
 import Controller from '@ember/controller';
 import type ShrimpoEntry from 'datafruits13/models/shrimpo-entry';
 import { formatEmojiHtml } from 'datafruits13/helpers/format-emoji-html';
+import { inject as service } from '@ember/service';
 
 export default class HomeShrimposEntriesShow extends Controller {
   declare model: ShrimpoEntry;
+  @service declare currentUser: any;
 
   get scoreEmoji() {
     return formatEmojiHtml(this.model.shrimpoEmoji);
