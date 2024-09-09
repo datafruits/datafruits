edd9428ecf2ade1bfca996d221d46b347eadf217
Tony Miller <mcfiredrill@gmail.com>
2024-09-08 21:51:52 -0700

    save all votes at once w fetch

--- app/components/shrimpo/voting-categories-table.ts
+++ app/components/shrimpo/voting-categories-table.ts
@@ -4,6 +4,8 @@ import type ShrimpoEntry from 'datafruits13/models/shrimpo-entry';
 import type ShrimpoVotingCategory from 'datafruits13/models/shrimpo-voting-category';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import ENV from 'datafruits13/config/environment';
+import fetch from 'fetch';
 
 interface ShrimpoVotingCategoriesTableArgs {
   entry: ShrimpoEntry;
@@ -22,20 +24,55 @@ export default class ShrimpoVotingCategoriesTable extends Component<ShrimpoVotin
 
   @tracked votes: any = {};
 
+  shrimpVoteUrl = `${ENV.API_HOST}/api/shrimpos/${this.args.entry.shrimpoSlug}/shrimpo_entries/${this.args.entry.slug}/shrimpo_votes.json`;
+
   constructor(owner: unknown, args: any) {
     super(owner, args);
     this.args.votingCategories.forEach((votingCategory: ShrimpoVotingCategory) => {
+      // TODO how to populate existing votes????
       this.votes[votingCategory.name] = {score: 0, emoji: votingCategory.emoji};
     });
   }
 
   @action
-  setScore(event: any, name: any) {
+  setScore(name: string, event: any) {
     this.votes[name].score = event.target.value;
   }
 
   @action
   async saveVote() {
+    const shrimpVotesData = Object.entries(this.votes).map((vote) => {
+      const attrs: any = vote[1];
+      return {
+        attributes: {
+          category_name: vote[0] as string,
+          score: attrs.score as number
+        },
+        type: "shrimpo_votes"
+      };
+    });
     // save all votes one by one ? or new API ? or new model ???
+    const data = {
+      data: shrimpVotesData
+    };
+
+    fetch(this.shrimpVoteUrl, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((data) => {
+        if (data.status == 200) {
+          alert('Voteded!');
+        } else {
+          alert('Something went wrong!');
+        }
+      })
+      .catch(() => {
+        alert('Something went wrong!');
+      });
   }
 }
