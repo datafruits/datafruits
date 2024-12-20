654776fdecd31e2bee63deb8ace9eb55a24073f1
Tony Miller <mcfiredrill@gmail.com>
2024-12-19 19:34:30 -0800

    mega shrimpo ui (#1384)
    * mega shrimpo form
    
    * add more already voted ui
    
    * set avatar height
    
    * show category results
    
    * fix category score display

--- app/components/shrimpo/entry-card.ts
+++ app/components/shrimpo/entry-card.ts
@@ -1,13 +1,20 @@
 import Component from '@glimmer/component';
 import type ShrimpoEntry from 'datafruits13/models/shrimpo-entry';
+import type ShrimpoVotingCategoryScore from 'datafruits13/models/shrimpo-voting-category-score';
 import { formatEmojiHtml } from 'datafruits13/helpers/format-emoji-html';
 
 interface ShrimpoEntryCardArgs {
   shrimpoEntry: ShrimpoEntry;
+  categoryScore: ShrimpoVotingCategoryScore;
 }
 
 export default class ShrimpoEntryCard extends Component<ShrimpoEntryCardArgs> {
   get scoreEmoji() {
-    return formatEmojiHtml(this.args.shrimpoEntry.shrimpoEmoji);
+    // need to use get here
+    return formatEmojiHtml(this.args.shrimpoEntry.get('shrimpoEmoji'));
+  }
+
+  get categoryScoreEmoji() {
+    return formatEmojiHtml(this.args.categoryScore.shrimpoVotingCategory.get('emoji'));
   }
 }
