2a5a150bbf9b97a54efe42d37497436462ab6809
Tony Miller <mcfiredrill@gmail.com>
2024-12-19 19:30:46 -0800

    fix category score display

--- app/models/shrimpo-voting-category.ts
+++ app/models/shrimpo-voting-category.ts
@@ -1,11 +1,15 @@
-import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany, type SyncHasMany } from '@ember-data/model';
 import type Shrimpo from './shrimpo';
 import type shrimpoVotingCategoryScore from './shrimpo-voting-category-score';
 
 export default class ShrimpoVotingCategory extends Model {
   @belongsTo('shrimpo') declare shrimpo: Shrimpo;
-  @hasMany('shrimpo-voting-category-score') declare shrimpoVotingCategoryScores: shrimpoVotingCategoryScore;
+  @hasMany('shrimpo-voting-category-score') declare shrimpoVotingCategoryScores: SyncHasMany<shrimpoVotingCategoryScore>;
 
   @attr('string') declare name: string;
   @attr('string') declare emoji: string;
+
+  get sortedScores() {
+    return this.shrimpoVotingCategoryScores.sortBy('ranking');
+  }
 }
