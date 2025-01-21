f2bffa09e27f1d3ad67c8d9889d205bc9d5b4d03
Tony Miller <mcfiredrill@gmail.com>
2025-01-21 09:31:25 -0800

    show hype meter

--- app/services/chat.ts
+++ app/services/chat.ts
@@ -39,6 +39,7 @@ export default class ChatService extends Service {
 
   @tracked _fruitCounts: FruitCount = {};
 
+  @tracked limitBreakActivated: boolean = true; // TODO
   @tracked limitBreakProgress: number = 0;
 
   @tracked loading: boolean = true;
@@ -277,6 +278,12 @@ export default class ChatService extends Service {
       console.log('limit break reached: ', msg);
       // this.eventBus.publish("limitBreakReached", msg.limitBreakCombo);
     });
+
+    // this.chan.on('limit_break_activate', (msg) => {
+    // });
+    //
+    // this.chan.on('limit_break_cooldown', (msg) => {
+    // });
 
     this.eventBus.subscribe('trackPlayed', this, 'onTrackPlayed');
 
