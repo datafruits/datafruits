cff3f0ab8f176f9e01e414e1ce8bbc10c60c6c0b
Tony Miller <mcfiredrill@gmail.com>
2025-02-17 13:29:12 -0800

    fix test

--- tests/integration/components/fruit-tip-test.js
+++ tests/integration/components/fruit-tip-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | fruit-tip', function (hooks) {
 
     await render(hbs`<FruitTip />`);
 
-    assert.dom(this.element).hasText('Lv. 3 Lv. 4 Lv. 5 Lv. 6 Lv. 7 Ƒ200 Ƒ400 Ƒ1000 Ƒ1200');
+    assert.dom(this.element).hasText('Lv. 3 Lv. 4 Lv. 5 Lv. 6 Lv. 7 Ƒ200 Ƒ400 Ƒ500 Ƒ1000 Ƒ1200');
   });
 
   skip('it shows fruit tipping options when clicked', async function (/*assert*/) {
