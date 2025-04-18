b003ad0608802eb2caf99709d45ae46b5b5a7392
Tony Miller <mcfiredrill@gmail.com>
2025-04-18 08:06:30 -0700

    fix notifications loader

--- app/routes/home/dj-inquiry.js
+++ app/routes/home/dj-inquiry.js
@@ -1,9 +1,11 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class DjInquiryRoute extends Route {
+  @service store;
   model() {
     let hostApplication;
-    hostApplication = this.store.peekAll('host-application').get('firstObject');
+    hostApplication = this.store.peekAll('host-application')[0];
     if (!hostApplication) {
       hostApplication = this.store.createRecord('host-application');
     }
