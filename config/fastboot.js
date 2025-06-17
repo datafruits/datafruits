8323fc331ed2cb389fc269026248becbec996afb
Tony Miller <mcfiredrill@gmail.com>
2025-06-17 09:46:44 -0700

    commit fastboot.js config

---
+++ config/fastboot.js
@@ -0,0 +1,21 @@
+/* eslint-disable n/no-unsupported-features/node-builtins */
+module.exports = function (environment) {
+  return {
+    buildSandboxGlobals(defaultGlobals) {
+      return Object.assign({}, defaultGlobals, {
+        fetch,
+        URLSearchParams,
+        AbortController,
+        DOMException,
+        Response,
+        ReadableStream:
+          typeof ReadableStream !== 'undefined' ? ReadableStream : require('node:stream/web').ReadableStream,
+        WritableStream:
+          typeof WritableStream !== 'undefined' ? WritableStream : require('node:stream/web').WritableStream,
+        TransformStream:
+          typeof TransformStream !== 'undefined' ? TransformStream : require('node:stream/web').TransformStream,
+        Headers: typeof Headers !== 'undefined' ? Headers : undefined,
+      });
+    },
+  };
+};
