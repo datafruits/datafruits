7ab0131e629a0cfc4d7970a9dd1a82fce9c3dd13
Tony Miller <mcfiredrill@gmail.com>
2025-06-30 09:54:17 -0700

    fix emoji path

--- app/helpers/emoji-path.ts
+++ app/helpers/emoji-path.ts
@@ -2,7 +2,7 @@ import { helper } from '@ember/component/helper';
 import emojione from 'emojione';
 
 export function emojiPath(args: any) {
-  return emojione.shortnameToPath(args[0]);
+  return emojione.shortnameToPath(args);
 }
 
 export default helper(emojiPath);
