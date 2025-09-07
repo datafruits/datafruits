1070861374eee40b167020cd9eaaf31b864d504b
Tony Miller <mcfiredrill@gmail.com>
2025-09-07 14:00:20 -0700

    doops

--- app/components/hype-meter.ts
+++ app/components/hype-meter.ts
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import type ChatService from 'datafruits13/services/chat';
 
-export default class HypeMeter extends Component<> {
+export default class HypeMeter extends Component {
   @service declare chat: ChatService;
   get progress() {
     return this.chat.limitBreakProgress;
