c2dedf3b0af0c2300e5fe7a41a0358d6e4653b6c
Tony Miller <mcfiredrill@gmail.com>
2025-09-07 13:58:48 -0700

    fix lint

--- app/components/hype-meter.ts
+++ app/components/hype-meter.ts
@@ -2,9 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import type ChatService from 'datafruits13/services/chat';
 
-interface HypeMeterArgs {}
-
-export default class HypeMeter extends Component<HypeMeterArgs> {
+export default class HypeMeter extends Component<> {
   @service declare chat: ChatService;
   get progress() {
     return this.chat.limitBreakProgress;
