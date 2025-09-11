008beb8ed0caaccb14b5a6467943eee477934bae
Dakota Clark <dakotamusic@gmail.com>
2025-09-10 21:49:05 -0700

    add helper for emoji marked down

---
+++ app/helpers/emoji-marked-down.js
@@ -0,0 +1,15 @@
+import { helper } from '@ember/component/helper';
+import showdown from 'showdown';
+import { htmlSafe } from '@ember/template';
+import { isBlank, isEmpty } from '@ember/utils';
+import emojione from 'emojione';
+
+export default helper(function markedDown(string) {
+  if (isEmpty(string) || isBlank(string)) {
+    return '';
+  }
+  const converter = new showdown.Converter({ simplifiedAutoLink: true });
+  return htmlSafe(
+    emojione.shortnameToImage(converter.makeHtml(string.toString()))
+  );
+});
