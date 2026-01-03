fff4d59149ff714f749cfd8e2bf8a269c242ba45
Tony Miller <mcfiredrill@gmail.com>
2026-01-03 13:30:51 -0800

    fix chat-preview

--- app/components/chat-preview.ts
+++ app/components/chat-preview.ts
@@ -1,9 +1,34 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import type ChatService from 'datafruits13/services/chat';
 
 interface ChatPreviewArgs {}
 
 export default class ChatPreview extends Component<ChatPreviewArgs> {
   @service declare chat: ChatService;
+  @tracked newMessagesBelow = false; // TODO move this to chat service
+
+  @action
+  onScroll() {
+    if (this.checkScrolledToBottom()) {
+      this.chat.isScrolledToBottom = true;
+      this.newMessagesBelow = false;
+    } else {
+      this.chat.isScrolledToBottom = false;
+      this.newMessagesBelow = true;
+    }
+  }
+
+  checkScrolledToBottom() {
+    const messages = document.getElementById('messages');
+    if(messages) {
+      const messagesHeight = messages.getBoundingClientRect().height;
+      return messages.scrollHeight - messages.scrollTop - messagesHeight < 1;
+    } else {
+      return false;
+    }
+  }
+
 }
